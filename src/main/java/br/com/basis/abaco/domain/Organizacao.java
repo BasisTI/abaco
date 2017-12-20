9b2912c63f683227bd94b5eb0310a1750b31bc68
Diego Marcilio <diego.marcilio@basis.com.br>
2017-12-20 17:47:08 -0200

    Organização salvando Contrato com cascade

--- src/main/java/br/com/basis/abaco/domain/Organizacao.java
+++ src/main/java/br/com/basis/abaco/domain/Organizacao.java
@@ -7,6 +7,7 @@ import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 import org.springframework.data.elasticsearch.annotations.Document;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -34,161 +35,154 @@ import java.util.Set;
 @Document(indexName = "organizacao")
 public class Organizacao implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
-    @SequenceGenerator(name = "sequenceGenerator")
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+	@SequenceGenerator(name = "sequenceGenerator")
+	private Long id;
 
-    @Size(max = 80)
-    @Column(name = "nome", length = 80)
-    private String nome;
+	@Size(max = 80)
+	@Column(name = "nome", length = 80)
+	private String nome;
 
-    @Size(max = 19)
-    @Pattern(regexp = "(^(\\d{2}.\\d{3}.\\d{3}/\\d{4}-\\d{2})|(\\d{14})$)")
-    @Column(name = "cnpj", length = 19)
-    private String cnpj;
+	@Size(max = 19)
+	@Pattern(regexp = "(^(\\d{2}.\\d{3}.\\d{3}/\\d{4}-\\d{2})|(\\d{14})$)")
+	@Column(name = "cnpj", length = 19)
+	private String cnpj;
 
-    @NotNull
-    @Column(name = "ativo", nullable = false)
-    private Boolean ativo;
+	@NotNull
+	@Column(name = "ativo", nullable = false)
+	private Boolean ativo;
 
-    @Column(name = "numero_ocorrencia")
-    private String numeroOcorrencia;
+	@Column(name = "numero_ocorrencia")
+	private String numeroOcorrencia;
 
+	@OneToMany(mappedBy = "organizacao")
+	@JsonIgnore
+	@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+	private Set<Sistema> sistemas = new HashSet<>();
 
-    @OneToMany(mappedBy = "organizacao")
-    @JsonIgnore
-    @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
-    private Set<Sistema> sistemas = new HashSet<>();
+	@OneToMany(mappedBy = "organization", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@JsonManagedReference
+	private Set<Contrato> contracts = new HashSet<>();
 
-    @OneToMany(mappedBy = "organization", fetch = FetchType.EAGER)
-    @JsonManagedReference
-    private Set<Contrato> contracts = new HashSet<>();
+	public Long getId() {
+		return id;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public String getNome() {
+		return nome;
+	}
 
-    public String getNome() {
-        return nome;
-    }
+	public Organizacao nome(String nome) {
+		this.nome = nome;
+		return this;
+	}
 
-    public Organizacao nome(String nome) {
-        this.nome = nome;
-        return this;
-    }
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+	public String getCnpj() {
+		return cnpj;
+	}
 
-    public String getCnpj() {
-        return cnpj;
-    }
+	public Organizacao cnpj(String cnpj) {
+		this.cnpj = cnpj;
+		return this;
+	}
 
-    public Organizacao cnpj(String cnpj) {
-        this.cnpj = cnpj;
-        return this;
-    }
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
 
-    public void setCnpj(String cnpj) {
-        this.cnpj = cnpj;
-    }
+	public Boolean isAtivo() {
+		return ativo;
+	}
 
-    public Boolean isAtivo() {
-        return ativo;
-    }
+	public Organizacao ativo(Boolean ativo) {
+		this.ativo = ativo;
+		return this;
+	}
 
-    public Organizacao ativo(Boolean ativo) {
-        this.ativo = ativo;
-        return this;
-    }
+	public void setAtivo(Boolean ativo) {
+		this.ativo = ativo;
+	}
 
-    public void setAtivo(Boolean ativo) {
-        this.ativo = ativo;
-    }
+	public String getNumeroOcorrencia() {
+		return numeroOcorrencia;
+	}
 
-    public String getNumeroOcorrencia() {
-        return numeroOcorrencia;
-    }
+	public Organizacao numeroOcorrencia(String numeroOcorrencia) {
+		this.numeroOcorrencia = numeroOcorrencia;
+		return this;
+	}
 
-    public Organizacao numeroOcorrencia(String numeroOcorrencia) {
-        this.numeroOcorrencia = numeroOcorrencia;
-        return this;
-    }
+	public void setNumeroOcorrencia(String numeroOcorrencia) {
+		this.numeroOcorrencia = numeroOcorrencia;
+	}
 
-    public void setNumeroOcorrencia(String numeroOcorrencia) {
-        this.numeroOcorrencia = numeroOcorrencia;
-    }
+	public Set<Sistema> getSistemas() {
+		return sistemas;
+	}
 
+	public Organizacao sistemas(Set<Sistema> sistemas) {
+		this.sistemas = sistemas;
+		return this;
+	}
 
-    public Set<Sistema> getSistemas() {
-        return sistemas;
-    }
+	public Organizacao addSistema(Sistema sistema) {
+		this.sistemas.add(sistema);
+		sistema.setOrganizacao(this);
+		return this;
+	}
 
-    public Organizacao sistemas(Set<Sistema> sistemas) {
-        this.sistemas = sistemas;
-        return this;
-    }
+	public Organizacao removeSistema(Sistema sistema) {
+		this.sistemas.remove(sistema);
+		sistema.setOrganizacao(null);
+		return this;
+	}
 
-    public Organizacao addSistema(Sistema sistema) {
-        this.sistemas.add(sistema);
-        sistema.setOrganizacao(this);
-        return this;
-    }
+	public void setSistemas(Set<Sistema> sistemas) {
+		this.sistemas = sistemas;
+	}
 
-    public Organizacao removeSistema(Sistema sistema) {
-        this.sistemas.remove(sistema);
-        sistema.setOrganizacao(null);
-        return this;
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		Organizacao organizacao = (Organizacao) o;
+		if (organizacao.id == null || id == null) {
+			return false;
+		}
+		return Objects.equals(id, organizacao.id);
+	}
 
-    public void setSistemas(Set<Sistema> sistemas) {
-        this.sistemas = sistemas;
-    }
+	public Set<Contrato> getContracts() {
+		return contracts;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Organizacao organizacao = (Organizacao) o;
-        if (organizacao.id == null || id == null) {
-            return false;
-        }
-        return Objects.equals(id, organizacao.id);
-    }
+	public void setContracts(Set<Contrato> contracts) {
+		this.contracts = contracts;
+	}
 
-    public Set<Contrato> getContracts() {
-        return contracts;
-    }
+	@Override
+	public int hashCode() {
+		return Objects.hashCode(id);
+	}
 
-    public void setContracts(Set<Contrato> contracts) {
-        this.contracts = contracts;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
-    }
-
-    @Override
-    public String toString() {
-        return "Organizacao{" +
-            "id=" + id +
-            ", nome='" + nome + "'" +
-            ", cnpj='" + cnpj + "'" +
-            ", ativo='" + ativo + "'" +
-            ", numeroOcorrencia='" + numeroOcorrencia + "'" +
-            '}';
-    }
+	@Override
+	public String toString() {
+		return "Organizacao{" + "id=" + id + ", nome='" + nome + "'" + ", cnpj='" + cnpj + "'" + ", ativo='" + ativo
+				+ "'" + ", numeroOcorrencia='" + numeroOcorrencia + "'" + '}';
+	}
 }
