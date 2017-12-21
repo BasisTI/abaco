59764027778e154233d7f550453f2d51ce04f721
Diego Marcilio <diego.marcilio@basis.com.br>
2017-12-21 11:21:59 -0200

    Esclarecendo alguns nomes de métodos em OrganizacaoService

--- src/app/organizacao/organizacao.service.ts
+++ src/app/organizacao/organizacao.service.ts
@@ -10,66 +10,62 @@ import { ResponseWrapper, createRequestOption, JhiDateUtils } from '../shared';
 @Injectable()
 export class OrganizacaoService {
 
-  resourceUrl = environment.apiUrl + '/organizacaos';
+  resourceName = '/organizacaos';
 
-  searchUrl = environment.apiUrl + '/_search/organizacaos';
+  resourceUrl = environment.apiUrl + this.resourceName;
+
+  searchUrl = environment.apiUrl + '/_search' + this.resourceName;
 
   constructor(private http: HttpService) {}
 
   create(organizacao: Organizacao): Observable<Organizacao> {
-    const copy = this.convert(organizacao);
+    const copy = this.convertToJSON(organizacao);
     return this.http.post(this.resourceUrl, copy).map((res: Response) => {
       const jsonResponse = res.json();
-      return this.convertItemFromServer(jsonResponse);
+      return this.convertFromJSON(jsonResponse);
     });
   }
 
   update(organizacao: Organizacao): Observable<Organizacao> {
-    const copy = this.convert(organizacao);
+    const copy = this.convertToJSON(organizacao);
     return this.http.put(this.resourceUrl, copy).map((res: Response) => {
       const jsonResponse = res.json();
-      return this.convertItemFromServer(jsonResponse);
+      return this.convertFromJSON(jsonResponse);
     });
   }
 
   find(id: number): Observable<Organizacao> {
     return this.http.get(`${this.resourceUrl}/${id}`).map((res: Response) => {
       const jsonResponse = res.json();
-      return this.convertItemFromServer(jsonResponse);
+      return this.convertFromJSON(jsonResponse);
     });
   }
 
   query(req?: any): Observable<ResponseWrapper> {
     const options = createRequestOption(req);
     return this.http.get(this.resourceUrl, options)
-      .map((res: Response) => this.convertResponse(res));
+      .map((res: Response) => this.convertResponseToResponseWrapper(res));
   }
 
   delete(id: number): Observable<Response> {
     return this.http.delete(`${this.resourceUrl}/${id}`);
   }
 
-  private convertResponse(res: Response): ResponseWrapper {
+  private convertResponseToResponseWrapper(res: Response): ResponseWrapper {
     const jsonResponse = res.json();
     const result = [];
     for (let i = 0; i < jsonResponse.length; i++) {
-      result.push(this.convertItemFromServer(jsonResponse[i]));
+      result.push(this.convertFromJSON(jsonResponse[i]));
     }
     return new ResponseWrapper(res.headers, result, res.status);
   }
 
-  /**
-   * Convert a returned JSON object to Organizacao.
-   */
-  private convertItemFromServer(json: any): Organizacao {
+  private convertFromJSON(json: any): Organizacao {
     const entity: Organizacao = Object.assign(new Organizacao(), json);
     return entity;
   }
 
-  /**
-   * Convert a Organizacao to a JSON which can be sent to the server.
-   */
-  private convert(organizacao: Organizacao): Organizacao {
+  private convertToJSON(organizacao: Organizacao): Organizacao {
     const copy: Organizacao = Object.assign({}, organizacao);
     return copy;
   }
