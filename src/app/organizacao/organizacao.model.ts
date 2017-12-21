6a42ce17702211e46d5276de3224c08fb26a7850
Diego Marcilio <diego.marcilio@basis.com.br>
2017-12-21 15:34:26 -0200

    Organização agora tem sigla

--- src/app/organizacao/organizacao.model.ts
+++ src/app/organizacao/organizacao.model.ts
@@ -5,6 +5,7 @@ export class Organizacao implements BaseEntity {
 
   constructor(
     public id?: number,
+    public sigla?: string,
     public nome?: string,
     public cnpj?: string,
     public ativo?: boolean,
