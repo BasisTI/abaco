1f98e153846779909d203fa479b9bdefcfbbf5fe
Diego Marcilio <diego.marcilio@basis.com.br>
2017-12-27 09:42:09 -0200

    Ajustando CRUD de 'TipoFase' de acordo com o protótipi

--- src/app/tipo-fase/tipo-fase.component.ts
+++ src/app/tipo-fase/tipo-fase.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ConfirmationService } from 'primeng/primeng';
 import { DatatableComponent, DatatableClickEvent } from '@basis/angular-components';
@@ -6,22 +6,30 @@ import { DatatableComponent, DatatableClickEvent } from '@basis/angular-componen
 import { environment } from '../../environments/environment';
 import { TipoFase } from './tipo-fase.model';
 import { TipoFaseService } from './tipo-fase.service';
+import { ElasticQuery } from '../shared';
 
 @Component({
   selector: 'jhi-tipo-fase',
   templateUrl: './tipo-fase.component.html'
 })
-export class TipoFaseComponent {
+export class TipoFaseComponent implements AfterViewInit {
 
   @ViewChild(DatatableComponent) datatable: DatatableComponent;
 
   searchUrl: string = this.tipoFaseService.searchUrl;
+
+  paginationParams = { contentIndex: null };
+  elasticQuery: ElasticQuery = new ElasticQuery();
 
   constructor(
     private router: Router,
     private tipoFaseService: TipoFaseService,
     private confirmationService: ConfirmationService
   ) {}
+
+  ngAfterViewInit() {
+    this.datatable.refresh(this.elasticQuery.query);
+  }
 
   datatableClick(event: DatatableClickEvent) {
     if (!event.selection) {
@@ -45,7 +53,7 @@ export class TipoFaseComponent {
       message: 'Tem certeza que deseja excluir o registro?',
       accept: () => {
         this.tipoFaseService.delete(id).subscribe(() => {
-          this.datatable.refresh(undefined);
+          this.datatable.refresh(this.elasticQuery.query);
         });
       }
     });
