fa9a57097fe101b7cdeef80a2f620cb5e7042c1f
Diego Marcilio <diego.marcilio@basis.com.br>
2017-12-27 17:02:20 -0200

    Alterando cascade / @JsonIgnore de Sistema -> Modulo -> Funcionalidade
    - Objetivo: suportar o novo jeito de cadastro (de acordo com layout)
    -- Módulo é cadastrado dentro de Sistema
    --- Funcionalidade é cadastrada na mesma tela

--- src/main/java/br/com/basis/abaco/domain/Modulo.java
+++ src/main/java/br/com/basis/abaco/domain/Modulo.java
@@ -1,10 +1,11 @@
 package br.com.basis.abaco.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import org.hibernate.annotations.Cache;
-import org.hibernate.annotations.CacheConcurrencyStrategy;
-import org.springframework.data.elasticsearch.annotations.Document;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -15,10 +16,12 @@ import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
-import java.io.Serializable;
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 /**
  * A Modulo.
@@ -29,109 +32,106 @@ import java.util.Set;
 @Document(indexName = "modulo")
 public class Modulo implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
-    @SequenceGenerator(name = "sequenceGenerator")
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+	@SequenceGenerator(name = "sequenceGenerator")
+	private Long id;
 
-    @NotNull
-    @Column(name = "nome", nullable = false)
-    private String nome;
+	@NotNull
+	@Column(name = "nome", nullable = false)
+	private String nome;
 
-    @ManyToOne
-    private Sistema sistema;
+	@ManyToOne
+	@JsonIgnore
+	private Sistema sistema;
 
-    @OneToMany(mappedBy = "modulo")
-    @JsonIgnore
-    @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
-    private Set<Funcionalidade> funcionalidades = new HashSet<>();
+	@OneToMany(mappedBy = "modulo", cascade = CascadeType.ALL)
+	@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+	private Set<Funcionalidade> funcionalidades = new HashSet<>();
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getNome() {
-        return nome;
-    }
+	public String getNome() {
+		return nome;
+	}
 
-    public Modulo nome(String nome) {
-        this.nome = nome;
-        return this;
-    }
+	public Modulo nome(String nome) {
+		this.nome = nome;
+		return this;
+	}
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-    public Sistema getSistema() {
-        return sistema;
-    }
+	public Sistema getSistema() {
+		return sistema;
+	}
 
-    public Modulo sistema(Sistema sistema) {
-        this.sistema = sistema;
-        return this;
-    }
+	public Modulo sistema(Sistema sistema) {
+		this.sistema = sistema;
+		return this;
+	}
 
-    public void setSistema(Sistema sistema) {
-        this.sistema = sistema;
-    }
+	public void setSistema(Sistema sistema) {
+		this.sistema = sistema;
+	}
 
-    public Set<Funcionalidade> getFuncionalidades() {
-        return funcionalidades;
-    }
+	public Set<Funcionalidade> getFuncionalidades() {
+		return funcionalidades;
+	}
 
-    public Modulo funcionalidades(Set<Funcionalidade> funcionalidades) {
-        this.funcionalidades = funcionalidades;
-        return this;
-    }
+	public Modulo funcionalidades(Set<Funcionalidade> funcionalidades) {
+		this.funcionalidades = funcionalidades;
+		return this;
+	}
 
-    public Modulo addFuncionalidade(Funcionalidade funcionalidade) {
-        this.funcionalidades.add(funcionalidade);
-        funcionalidade.setModulo(this);
-        return this;
-    }
+	public Modulo addFuncionalidade(Funcionalidade funcionalidade) {
+		this.funcionalidades.add(funcionalidade);
+		funcionalidade.setModulo(this);
+		return this;
+	}
 
-    public Modulo removeFuncionalidade(Funcionalidade funcionalidade) {
-        this.funcionalidades.remove(funcionalidade);
-        funcionalidade.setModulo(null);
-        return this;
-    }
+	public Modulo removeFuncionalidade(Funcionalidade funcionalidade) {
+		this.funcionalidades.remove(funcionalidade);
+		funcionalidade.setModulo(null);
+		return this;
+	}
 
-    public void setFuncionalidades(Set<Funcionalidade> funcionalidades) {
-        this.funcionalidades = funcionalidades;
-    }
+	public void setFuncionalidades(Set<Funcionalidade> funcionalidades) {
+		this.funcionalidades = funcionalidades;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Modulo modulo = (Modulo) o;
-        if (modulo.id == null || id == null) {
-            return false;
-        }
-        return Objects.equals(id, modulo.id);
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		Modulo modulo = (Modulo) o;
+		if (modulo.id == null || id == null) {
+			return false;
+		}
+		return Objects.equals(id, modulo.id);
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
-    }
+	@Override
+	public int hashCode() {
+		return Objects.hashCode(id);
+	}
 
-    @Override
-    public String toString() {
-        return "Modulo{" +
-            "id=" + id +
-            ", nome='" + nome + "'" +
-            '}';
-    }
+	@Override
+	public String toString() {
+		return "Modulo{" + "id=" + id + ", nome='" + nome + "'" + '}';
+	}
 }
--- src/main/java/br/com/basis/abaco/domain/Sistema.java
+++ src/main/java/br/com/basis/abaco/domain/Sistema.java
@@ -1,10 +1,11 @@
 package br.com.basis.abaco.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import org.hibernate.annotations.Cache;
-import org.hibernate.annotations.CacheConcurrencyStrategy;
-import org.springframework.data.elasticsearch.annotations.Document;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -16,10 +17,10 @@ import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.io.Serializable;
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.springframework.data.elasticsearch.annotations.Document;
 
 /**
  * A Sistema.
@@ -30,144 +31,139 @@ import java.util.Set;
 @Document(indexName = "sistema")
 public class Sistema implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
-    @SequenceGenerator(name = "sequenceGenerator")
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+	@SequenceGenerator(name = "sequenceGenerator")
+	private Long id;
 
-    @Size(max = 20)
-    @Column(name = "sigla", length = 20)
-    private String sigla;
+	@Size(max = 20)
+	@Column(name = "sigla", length = 20)
+	private String sigla;
 
-    @NotNull
-    @Column(name = "nome", nullable = false)
-    private String nome;
+	@NotNull
+	@Column(name = "nome", nullable = false)
+	private String nome;
 
-    @Column(name = "numero_ocorrencia")
-    private String numeroOcorrencia;
+	@Column(name = "numero_ocorrencia")
+	private String numeroOcorrencia;
 
-    @ManyToOne
-    private Organizacao organizacao;
+	@ManyToOne
+	private Organizacao organizacao;
 
-    @OneToMany(mappedBy = "sistema")
-    @JsonIgnore
-    @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
-    private Set<Modulo> modulos = new HashSet<>();
+	@OneToMany(mappedBy = "sistema", cascade = CascadeType.ALL)
+	@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+	private Set<Modulo> modulos = new HashSet<>();
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getSigla() {
-        return sigla;
-    }
+	public String getSigla() {
+		return sigla;
+	}
 
-    public Sistema sigla(String sigla) {
-        this.sigla = sigla;
-        return this;
-    }
+	public Sistema sigla(String sigla) {
+		this.sigla = sigla;
+		return this;
+	}
 
-    public void setSigla(String sigla) {
-        this.sigla = sigla;
-    }
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
 
-    public String getNome() {
-        return nome;
-    }
+	public String getNome() {
+		return nome;
+	}
 
-    public Sistema nome(String nome) {
-        this.nome = nome;
-        return this;
-    }
+	public Sistema nome(String nome) {
+		this.nome = nome;
+		return this;
+	}
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-    public String getNumeroOcorrencia() {
-        return numeroOcorrencia;
-    }
+	public String getNumeroOcorrencia() {
+		return numeroOcorrencia;
+	}
 
-    public Sistema numeroOcorrencia(String numeroOcorrencia) {
-        this.numeroOcorrencia = numeroOcorrencia;
-        return this;
-    }
+	public Sistema numeroOcorrencia(String numeroOcorrencia) {
+		this.numeroOcorrencia = numeroOcorrencia;
+		return this;
+	}
 
-    public void setNumeroOcorrencia(String numeroOcorrencia) {
-        this.numeroOcorrencia = numeroOcorrencia;
-    }
+	public void setNumeroOcorrencia(String numeroOcorrencia) {
+		this.numeroOcorrencia = numeroOcorrencia;
+	}
 
-    public Organizacao getOrganizacao() {
-        return organizacao;
-    }
+	public Organizacao getOrganizacao() {
+		return organizacao;
+	}
 
-    public Sistema organizacao(Organizacao organizacao) {
-        this.organizacao = organizacao;
-        return this;
-    }
+	public Sistema organizacao(Organizacao organizacao) {
+		this.organizacao = organizacao;
+		return this;
+	}
 
-    public void setOrganizacao(Organizacao organizacao) {
-        this.organizacao = organizacao;
-    }
+	public void setOrganizacao(Organizacao organizacao) {
+		this.organizacao = organizacao;
+	}
 
-    public Set<Modulo> getModulos() {
-        return modulos;
-    }
+	public Set<Modulo> getModulos() {
+		return modulos;
+	}
 
-    public Sistema modulos(Set<Modulo> modulos) {
-        this.modulos = modulos;
-        return this;
-    }
+	public Sistema modulos(Set<Modulo> modulos) {
+		this.modulos = modulos;
+		return this;
+	}
 
-    public Sistema addModulo(Modulo modulo) {
-        this.modulos.add(modulo);
-        modulo.setSistema(this);
-        return this;
-    }
+	public Sistema addModulo(Modulo modulo) {
+		this.modulos.add(modulo);
+		modulo.setSistema(this);
+		return this;
+	}
 
-    public Sistema removeModulo(Modulo modulo) {
-        this.modulos.remove(modulo);
-        modulo.setSistema(null);
-        return this;
-    }
+	public Sistema removeModulo(Modulo modulo) {
+		this.modulos.remove(modulo);
+		modulo.setSistema(null);
+		return this;
+	}
 
-    public void setModulos(Set<Modulo> modulos) {
-        this.modulos = modulos;
-    }
+	public void setModulos(Set<Modulo> modulos) {
+		this.modulos = modulos;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Sistema sistema = (Sistema) o;
-        if (sistema.id == null || id == null) {
-            return false;
-        }
-        return Objects.equals(id, sistema.id);
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		Sistema sistema = (Sistema) o;
+		if (sistema.id == null || id == null) {
+			return false;
+		}
+		return Objects.equals(id, sistema.id);
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
-    }
+	@Override
+	public int hashCode() {
+		return Objects.hashCode(id);
+	}
 
-    @Override
-    public String toString() {
-        return "Sistema{" +
-            "id=" + id +
-            ", sigla='" + sigla + "'" +
-            ", nome='" + nome + "'" +
-            ", numeroOcorrencia='" + numeroOcorrencia + "'" +
-            '}';
-    }
+	@Override
+	public String toString() {
+		return "Sistema{" + "id=" + id + ", sigla='" + sigla + "'" + ", nome='" + nome + "'" + ", numeroOcorrencia='"
+				+ numeroOcorrencia + "'" + '}';
+	}
 }
