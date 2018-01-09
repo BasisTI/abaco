b5d51534325a1a64578d55d29a4dcb231446b3af
Diego Marcilio <diego.marcilio@basis.com.br>
2018-01-09 13:16:52 -0200

    Reduzindo um pouco a duplicação dos testes

--- src/app/shared/mappable-entities.spec.ts
+++ src/app/shared/mappable-entities.spec.ts
@@ -1,99 +1,116 @@
 import { BaseEntity } from './base-entity';
 import { MappableEntities } from './mappable-entities';
 
+import * as _ from 'lodash';
+
 fdescribe('MappableEntities', () => {
 
     // tslint:disable-next-line:prefer-const
     let mappableEntities: MappableEntities<BaseEntity>;
+    let entityWithNoIds;
 
-    beforeEach(() => this.mappableEntities = new MappableEntities<BaseEntity>());
+    // push() is impure (modifies the argument). reinstantiating entityWithNoIds before each test case
+    beforeEach(() => {
+        mappableEntities = new MappableEntities<BaseEntity>();
+        entityWithNoIds = { name: 'entity', id: undefined, artificialId: undefined };
+    });
+
 
     describe('Instantiating with an initial list', () => {
         it('should add all values passed in the constructor', () => {
-            const e = { name: 'entity', id: undefined, artificialId: undefined };
             const e2 = { name: 'entity2', id: 2, artificialId: undefined };
-            const e3 = { name: 'entity3', id: undefined, artificialId: undefined };
+            const e3 = _.clone(entityWithNoIds);
+            e3.artificialId = 3;
 
-            this.mappableEntities = new MappableEntities<BaseEntity>([e, e2, e3]);
+            mappableEntities = new MappableEntities<BaseEntity>([entityWithNoIds, e2, e3]);
 
-            const values = this.mappableEntities.values();
-            expect(values).toContain(e);
-            expect(values).toContain(e2);
-            expect(values).toContain(e3);
-            expect(values.length).toEqual(3);
+            const values = mappableEntities.values();
+            expectToContainExactly(values, entityWithNoIds, e2, e3);
         });
     });
 
-    describe('Valid push scenarios', () => {
+    function expectToContainExactly(arr, ...values) {
+        expect(arr.length).toEqual(values.length);
+        doExpectToContain(arr, values);
+    }
+
+    function doExpectToContain(arr, values) {
+        for (const value of values) {
+            expect(arr).toContain(value);
+        }
+    }
+
+    function expectToContain(arr, ...values) {
+        doExpectToContain(arr, values);
+    }
+
+    describe('Push scenarios', () => {
+        let entity;
+
+        beforeEach(() => entity = entityWithNoIds);
 
         it('should add a BaseEntity with no artificialId nor id', () => {
-            const entity = { name: 'entity', id: undefined, artificialId: undefined };
-            this.mappableEntities.push(entity);
-            const values = this.mappableEntities.values();
+            mappableEntities.push(entity);
+            const values = mappableEntities.values();
             expect(values).toContain(entity);
         });
 
         it(`should generate and assign (impure) a artificialId to a BaseEntity with no
-        artificialId nor id`, () => {
-                const entity = { name: 'entity', id: undefined, artificialId: undefined };
-                this.mappableEntities.push(entity);
+            artificialId nor id`, () => {
+                mappableEntities.push(entity);
                 expect(entity.artificialId).not.toBeUndefined();
             });
 
         it('should not generate an artificialId to a BaseEntity with an id', () => {
-            const entity = { name: 'entity', id: 123, artificialId: undefined };
-            this.mappableEntities.push(entity);
+            entity.id = 123;
+            mappableEntities.push(entity);
             expect(entity.artificialId).toBeUndefined();
         });
 
         it('should not generate an artificialId to a BaseEntity with id 0', () => {
-            const e = { name: 'entity', id: 0, artificialId: undefined };
-            this.mappableEntities.push(e);
-            expect(e.artificialId).toBeUndefined();
+            entity.id = 0;
+            mappableEntities.push(entity);
+            expect(entity.artificialId).toBeUndefined();
         });
 
         it('should not change the id of a BaseEntity with and id', () => {
             const id = 123;
-            const entity = { name: 'entity', id: id, artificialId: undefined };
-            this.mappableEntities.push(entity);
+            entity.id = id;
+            mappableEntities.push(entity);
             expect(entity.id).toEqual(id);
         });
-    });
 
-    describe('Invalid push scenarios', () => {
         it(`should throw error when adding BaseEntity with same id as an
          already added one`, () => {
                 const id = 123;
-                const e = { name: 'entity', id: id, artificialId: undefined };
-                this.mappableEntities.push(e);
+                entity.id = id;
+                mappableEntities.push(entity);
 
                 const eWithSameId = { name: 'entity2', id: id, artificialId: undefined };
                 expect(() => {
-                    this.mappableEntities.push(eWithSameId);
+                    mappableEntities.push(eWithSameId);
                 }).toThrowError(RangeError);
 
-                const values = this.mappableEntities.values();
+                const values = mappableEntities.values();
                 expect(values.length).toEqual(1);
-                expect(values).toContain(e);
+                expect(values).toContain(entity);
                 expect(values).not.toContain(eWithSameId);
             });
 
         it(`should not generate an Id that already exists as a key`, () => {
-            const nextId: number = this.mappableEntities.nextGeneratedId();
+            const nextId: number = mappableEntities.nextGeneratedId();
 
-            const e = { name: 'entity', id: nextId, artificialId: undefined };
-            this.mappableEntities.push(e);
+            entity.id = nextId;
+            mappableEntities.push(entity);
 
             const e2 = { name: 'entity2', id: undefined, artificialId: undefined };
-            this.mappableEntities.push(e2);
+            mappableEntities.push(e2);
 
             expect(e2.artificialId).not.toEqual(nextId);
             expect(e2.artificialId).not.toBeUndefined();
 
-            const values = this.mappableEntities.values();
-            expect(values.length).toEqual(2);
-            expect(values).toContain(e);
-            expect(values).toContain(e2);
+            const values = mappableEntities.values();
+            expectToContainExactly(values, entity, e2);
         });
     });
 
