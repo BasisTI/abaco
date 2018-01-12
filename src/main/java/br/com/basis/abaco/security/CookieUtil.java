eeb7f064a5fa090ddf0b1b0d5414c1f64814dc32
Diego Marcilio <diego.marcilio@basis.com.br>
2018-01-12 17:46:25 -0200

    BASIS-29707 FIXME para quando ambiente não for desenvolvimento

--- src/main/java/br/com/basis/abaco/security/CookieUtil.java
+++ src/main/java/br/com/basis/abaco/security/CookieUtil.java
@@ -8,26 +8,16 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.util.WebUtils;
 
 @Component
+// FIXME (diego.marcilio) Funcionando somente para desenvolvimento
+// rever AuthenticationConstants setDomain(), setHttpOnly(), setSecure() 
 public class CookieUtil {
-	
-	/**
-	 * Método que cria o cookie de autenticação no sistema.
-	 * 
-	 * @param httpServletResponse
-	 *            HttpServletResponse
-	 * @param value
-	 *            valor do cookie
-	 * @param secure
-	 *            define se o cookie é 'secure'
-	 * @param maxAge
-	 *            validade do cookie
-	 */
+
 	public void create(HttpServletResponse httpServletResponse, String value, Boolean secure, Integer maxAge) {
 		Cookie cookie = new Cookie(AuthenticationConstants.TOKEN_NAME, value);
 		cookie.setSecure(secure);
 		cookie.setHttpOnly(false);
 		cookie.setMaxAge(maxAge);
-//		cookie.setDomain(DOMAIN);
+		// cookie.setDomain(DOMAIN);
 		cookie.setPath("/");
 		httpServletResponse.addCookie(cookie);
 	}
@@ -37,29 +27,15 @@ public class CookieUtil {
 				AuthenticationConstants.DEFAULT_MAX_AGE);
 	}
 
-	/**
-	 * Método que invalida os cookie de autenticação.
-	 * 
-	 * @param httpServletResponse
-	 *            httpServletResponse
-	 * @param httpServletRequest
-	 *            httpServletRequest
-	 */
 	public void clear(HttpServletResponse httpServletResponse) {
 		Cookie cookieJWT = new Cookie(AuthenticationConstants.TOKEN_NAME, null);
 		cookieJWT.setPath("/");
 		cookieJWT.setHttpOnly(false);
 		cookieJWT.setMaxAge(0);
-//		cookieJWT.setDomain(DOMAIN);
+		// cookieJWT.setDomain(DOMAIN);
 		httpServletResponse.addCookie(cookieJWT);
 	}
 
-	/**
-	 * Método que retorna o valor do cookie de autenticação
-	 * 
-	 * @param httpServletRequest
-	 * @return valor do cookie de autenticação
-	 */
 	public String getValue(HttpServletRequest httpServletRequest) {
 		Cookie cookie = WebUtils.getCookie(httpServletRequest, AuthenticationConstants.TOKEN_NAME);
 		return cookie != null ? cookie.getValue() : null;
