d334af854b7b77be186b77e6cc5bd6c2b313b404
Diego Marcilio <diego.marcilio@basis.com.br>
2018-01-15 10:05:19 -0200

    BASIS-29707 1o e último nome do usuário no BasisUserDetails

--- src/main/java/br/com/basis/abaco/security/BasisUserDetails.java
+++ src/main/java/br/com/basis/abaco/security/BasisUserDetails.java
@@ -8,12 +8,30 @@ import org.springframework.security.core.GrantedAuthority;
 
 public class BasisUserDetails {
 
+	private final String firstName;
+
+	private final String lastName;
+
 	private final List<String> roles;
 
-	public BasisUserDetails(Collection<? extends GrantedAuthority> roles) {
-		this.roles = roles.stream() //
+	public BasisUserDetails(String firstName, String lastName, Collection<? extends GrantedAuthority> roles) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.roles = collectRolesAsStrings(roles);
+	}
+
+	private List<String> collectRolesAsStrings(Collection<? extends GrantedAuthority> roles) {
+		return roles.stream() //
 				.map(GrantedAuthority::getAuthority) //
 				.collect(Collectors.toList());
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
 	}
 
 	public Collection<String> getRoles() {
--- src/main/java/br/com/basis/abaco/web/rest/UserJWTController.java
+++ src/main/java/br/com/basis/abaco/web/rest/UserJWTController.java
@@ -1,6 +1,7 @@
 package br.com.basis.abaco.web.rest;
 
 import java.net.URISyntaxException;
+import java.util.Collection;
 import java.util.Collections;
 
 import javax.servlet.http.HttpServletResponse;
@@ -14,9 +15,8 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,12 +26,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.codahale.metrics.annotation.Timed;
 
+import br.com.basis.abaco.domain.User;
 import br.com.basis.abaco.security.AuthenticationConstants;
 import br.com.basis.abaco.security.BasisUserDetails;
 import br.com.basis.abaco.security.CookieUtil;
 import br.com.basis.abaco.security.SecurityUtils;
 import br.com.basis.abaco.security.jwt.JWTConfigurer;
 import br.com.basis.abaco.security.jwt.TokenProvider;
+import br.com.basis.abaco.service.UserService;
 import br.com.basis.abaco.web.rest.vm.LoginVM;
 
 @RestController
@@ -44,14 +46,15 @@ public class UserJWTController {
 
 	private final AuthenticationManager authenticationManager;
 
+	private UserService userService;
+
 	private CookieUtil cookieUtil;
 
-	private UserDetailsService userDetailsService;
-
 	public UserJWTController(TokenProvider tokenProvider, AuthenticationManager authenticationManager,
-			CookieUtil cookieUtil) {
+			UserService userService, CookieUtil cookieUtil) {
 		this.tokenProvider = tokenProvider;
 		this.authenticationManager = authenticationManager;
+		this.userService = userService;
 		this.cookieUtil = cookieUtil;
 	}
 
@@ -69,7 +72,6 @@ public class UserJWTController {
 			String jwt = tokenProvider.createToken(authentication, rememberMe);
 			response.addHeader(JWTConfigurer.AUTHORIZATION_HEADER, "Bearer " + jwt);
 
-			// Cookie
 			cookieUtil.create(response, jwt);
 
 			return ResponseEntity.ok(new JWTToken(jwt));
@@ -86,10 +88,15 @@ public class UserJWTController {
 			@CookieValue(name = AuthenticationConstants.TOKEN_NAME, defaultValue = "notFound") String token)
 			throws URISyntaxException {
 		if (!token.equals("notFound")) {
-			BasisUserDetails userDetails = new BasisUserDetails(SecurityUtils.getCurrentUserRoles());
-			return ResponseEntity.ok(userDetails);
+			return ResponseEntity.ok(generateUserDetails());
 		}
 
 		return null;
+	}
+
+	private BasisUserDetails generateUserDetails() {
+		User currentUser = userService.getUserWithAuthorities();
+		Collection<? extends GrantedAuthority> currentUserRoles = SecurityUtils.getCurrentUserRoles();
+		return new BasisUserDetails(currentUser.getFirstName(), currentUser.getLastName(), currentUserRoles);
 	}
 }
