58b65198ecfa0c2762587929a2353673e410d108
Diego Marcilio <diego.marcilio@basis.com.br>
2018-01-15 17:10:45 -0200

    BASIS-29707 AuthGuard para 'tipoFase'

--- src/app/tipo-fase/tipo-fase.route.ts
+++ src/app/tipo-fase/tipo-fase.route.ts
@@ -5,21 +5,27 @@ import { TipoFaseComponent } from './tipo-fase.component';
 import { TipoFaseDetailComponent } from './tipo-fase-detail.component';
 import { TipoFaseFormComponent } from './tipo-fase-form.component';
 
+import { AuthGuard } from '@basis/angular-components';
+
 export const tipoFaseRoute: Routes = [
   {
     path: 'tipoFase',
-    component: TipoFaseComponent
+    component: TipoFaseComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: 'tipoFase/new',
-    component: TipoFaseFormComponent
+    component: TipoFaseFormComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: 'tipoFase/:id/edit',
-    component: TipoFaseFormComponent
+    component: TipoFaseFormComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: 'tipoFase/:id',
-    component: TipoFaseDetailComponent
+    component: TipoFaseDetailComponent,
+    canActivate: [AuthGuard]
   },
 ];
