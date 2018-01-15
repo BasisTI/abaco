a1b08270df0dec4a035c3a2f035d17afde13e524
Diego Marcilio <diego.marcilio@basis.com.br>
2018-01-15 17:12:56 -0200

    BASIS-29707 AuthGuard para 'manual'

--- src/app/manual/manual.route.ts
+++ src/app/manual/manual.route.ts
@@ -5,21 +5,27 @@ import { ManualComponent } from './manual.component';
 import { ManualDetailComponent } from './manual-detail.component';
 import { ManualFormComponent } from './manual-form.component';
 
+import { AuthGuard } from '@basis/angular-components';
+
 export const manualRoute: Routes = [
   {
     path: 'manual',
-    component: ManualComponent
+    component: ManualComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: 'manual/new',
-    component: ManualFormComponent
+    component: ManualFormComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: 'manual/:id/edit',
-    component: ManualFormComponent
+    component: ManualFormComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: 'manual/:id',
-    component: ManualDetailComponent
+    component: ManualDetailComponent,
+    canActivate: [AuthGuard]
   },
 ];
