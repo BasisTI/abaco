f481d24028926852156410ee7aeecde896c7b750
Diego Marcilio <diego.marcilio@basis.com.br>
2018-01-18 13:39:51 -0200

    Página login definida como home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes, RouterModule } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
+import { LoginComponent } from './login';
 
 export const routes: Routes = [
-  // {path: '', component: DashboardDemo}
+  { path: '', component: LoginComponent}
 ];
 
 export const AppRoutes: ModuleWithProviders = RouterModule.forRoot(routes);
