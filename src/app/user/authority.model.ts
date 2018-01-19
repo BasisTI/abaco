03e35ffb0c303650f89c19ea672e0dd2f8bf5b66
Diego Marcilio <diego.marcilio@basis.com.br>
2018-01-19 12:55:31 -0200

    multiselect soluçao hardcoded funcionando

--- src/app/user/authority.model.ts
+++ src/app/user/authority.model.ts
@@ -1,6 +1,14 @@
 export class Authority {
     constructor(
         public name?: string,
-    ) {}
+        public artificialId?: number,
+    ) {
+        if ('ROLE_ADMIN' === name) {
+            this.artificialId = 1;
+        }
+        if ('ROLE_USER' === name) {
+            this.artificialId = 2;
+        }
+    }
 
 }
