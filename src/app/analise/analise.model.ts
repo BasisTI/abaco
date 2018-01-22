b9cce42547168b1c1cea6bc76f245c0f8253ee91
Diego Marcilio <diego.marcilio@basis.com.br>
2018-01-22 08:59:02 -0200

    Aproveitando gerador para alguns arquivos
    - Nem todos sao aplicaveis

--- src/app/analise/analise.model.ts
+++ src/app/analise/analise.model.ts
@@ -1,7 +1,31 @@
 import { BaseEntity } from '../shared';
 
+export const enum MetodoContagem {
+  'DETALHADA',
+  'INDICATIVA',
+  'ESTIMADA'
+}
+export const enum TipoAnalise {
+  'DESENVOLVIMENTO',
+  'MELHORIA',
+  'APLICACAO'
+}
+
 export class Analise implements BaseEntity {
 
   constructor(
+    public id?: number,
+    public numeroOs?: string,
+    public tipoContagem?: MetodoContagem,
+    public valorAjuste?: number,
+    public pfTotal?: string,
+    public escopo?: string,
+    public fronteiras?: string,
+    public documentacao?: string,
+    public tipoAnalise?: TipoAnalise,
+    public propositoContagem?: string,
+    public sistema?: BaseEntity,
+    public funcaoDados?: BaseEntity[],
+    public funcaoTransacaos?: BaseEntity[],
   ) {}
 }
