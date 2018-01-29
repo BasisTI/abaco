ac9b03ff9e013993f52c79426a472d271b434ad8
Diego Marcilio <diego.marcilio@basis.com.br>
2018-01-29 16:13:45 -0200

    der-text-parser identacao de 2 espacos

--- src/app/analise-shared/der-text-parser.ts
+++ src/app/analise-shared/der-text-parser.ts
@@ -1,25 +1,25 @@
 export class ParseResult {
 
-    static readonly NUMERO_TIPO = 'NUMERO';
-    static readonly TEXTO_TIPO = 'TEXTO';
+  static readonly NUMERO_TIPO = 'NUMERO';
+  static readonly TEXTO_TIPO = 'TEXTO';
 
-    readonly tipo: string;
+  readonly tipo: string;
 
-    readonly numero: number;
+  readonly numero: number;
 
-    readonly textos: string[];
+  readonly textos: string[];
 
-    constructor(tipo: string, numero?: number, textos?: string[]) {
-        this.tipo = tipo;
-        this.numero = numero;
-        this.textos = textos;
-    }
+  constructor(tipo: string, numero?: number, textos?: string[]) {
+    this.tipo = tipo;
+    this.numero = numero;
+    this.textos = textos;
+  }
 }
 
 export class DerTextParser {
 
 
-    static parse(entrada: string): ParseResult {
-        return undefined;
-    }
+  static parse(entrada: string): ParseResult {
+    return undefined;
+  }
 }
