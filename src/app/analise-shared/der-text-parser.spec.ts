39ad8c14a70c5350347846af90f9704276fdb720
Diego Marcilio <diego.marcilio@basis.com.br>
2018-01-29 16:41:31 -0200

    der-text-parser primeiros testes para entrada textual

--- src/app/analise-shared/der-text-parser.spec.ts
+++ src/app/analise-shared/der-text-parser.spec.ts
@@ -26,4 +26,40 @@ fdescribe('DerTextParser', () => {
 
   });
 
+  describe('entrada textual', () => {
+
+    let result: ParseResult;
+    const entrada = `linha 1
+        linha 2
+        linha 3`;
+
+    beforeEach(() => {
+      result = DerTextParser.parse(entrada);
+    });
+
+    it(`deve retornar um ParseResult com tipo 'TEXTO'`, () => {
+      expect(result.tipo).toEqual(ParseResult.TEXTO_TIPO);
+    });
+
+    it('deve quebrar cada linha em um valor', () => {
+      expect(result.textos.length).toEqual(3);
+    });
+
+    it('deve conter cada linha com valor correto', () => {
+      expectToContainExactly(result.textos, 'linha 1', 'linha 2', 'linha 3');
+    });
+
+  });
+
+  function expectToContainExactly(arr, ...values) {
+    expect(arr.length).toEqual(values.length);
+    doExpectToContain(arr, values);
+  }
+
+  function doExpectToContain(arr, values) {
+    for (const value of values) {
+      expect(arr).toContain(value);
+    }
+  }
+
 });
