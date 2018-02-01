dc47d458c8e4ff5f9cb74b703e13f17c7abb17d0
Diego Marcilio <diego.marcilio@basis.com.br>
2018-02-01 15:07:46 -0200

    funcao-transacao aba comecando a tomar corpo
    - basicamente tudo clonado, com variacoes, de funcao-dados

--- src/app/funcao-transacao/funcao-transacao-form.component.ts
+++ src/app/funcao-transacao/funcao-transacao-form.component.ts
@@ -18,8 +18,75 @@ import { ConfirmationService } from 'primeng/primeng';
 })
 export class FuncaoTransacaoFormComponent implements OnInit {
 
-  ngOnInit() {
+  currentFuncaoTransacao: FuncaoTransacao;
+  funcaoTransacaoEmEdicao: FuncaoTransacao;
 
+  fatoresAjuste: FatorAjuste[] = [];
+
+  constructor(
+    private analiseSharedDataService: AnaliseSharedDataService,
+    private confirmationService: ConfirmationService,
+    private pageNotificationService: PageNotificationService
+  ) { }
+
+  ngOnInit() {
+    this.currentFuncaoTransacao = new FuncaoTransacao();
+  }
+
+  get funcoesTransacoes(): FuncaoTransacao[] {
+    if (!this.analise.funcaoTransacaos) {
+      return [];
+    }
+    return this.analise.funcaoTransacaos;
+  }
+
+  private get analise(): Analise {
+    return this.analiseSharedDataService.analise;
+  }
+
+  private set analise(analise: Analise) {
+    this.analiseSharedDataService.analise = analise;
+  }
+
+  private get manual() {
+    if (this.analiseSharedDataService.analise.contrato) {
+      return this.analiseSharedDataService.analise.contrato.manual;
+    }
+    return undefined;
+  }
+
+  isContratoSelected(): boolean {
+    // FIXME p-dropdown requer 2 clicks quando o [options] chama um método get()
+    const isContratoSelected = this.analiseSharedDataService.isContratoSelected();
+    if (isContratoSelected && this.fatoresAjuste.length === 0) {
+      this.fatoresAjuste = this.manual.fatoresAjuste;
+    }
+
+    return isContratoSelected;
+  }
+
+  fatoresAjusteDropdownPlaceholder() {
+    if (this.isContratoSelected()) {
+      return 'Valor de Ajuste';
+    } else {
+      return `Valor de Ajuste - Selecione um Contrato na aba 'Geral' para carregar os Valores de Ajuste`;
+    }
+  }
+
+  moduloSelected(modulo: Modulo) {
+  }
+
+  funcionalidadeSelected(funcionalidade: Funcionalidade) {
+    this.currentFuncaoTransacao.funcionalidade = funcionalidade;
+  }
+
+  isFuncionalidadeSelected(): boolean {
+    return !_.isUndefined(this.currentFuncaoTransacao.funcionalidade);
+  }
+
+  deveHabilitarBotaoAdicionar(): boolean {
+    // TODO complementar com outras validacoes
+    return this.isFuncionalidadeSelected();
   }
 
 }
