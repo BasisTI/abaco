be4e1eb2b4ed9ad6c1ee25915ff7f2e59441fcb3
Diego Marcilio <diego.marcilio@basis.com.br>
2018-02-01 14:32:09 -0200

    funcao-transacao adicionando atributos ao model

--- src/app/funcao-transacao/funcao-transacao.model.ts
+++ src/app/funcao-transacao/funcao-transacao.model.ts
@@ -1,4 +1,6 @@
 import { BaseEntity } from '../shared';
+import { FatorAjuste } from '../fator-ajuste/index';
+import { Funcionalidade } from '../funcionalidade/index';
 
 export const enum TipoFuncaoTransacao {
   'EE',
@@ -16,12 +18,26 @@ export class FuncaoTransacao implements BaseEntity {
 
   constructor(
     public id?: number,
+    public artificialId?: number,
     public tipo?: TipoFuncaoTransacao,
     public complexidade?: Complexidade,
     public pf?: number,
     public analise?: BaseEntity,
     public funcionalidades?: BaseEntity[],
-    public fatorAjuste?: BaseEntity,
+    public funcionalidade?: Funcionalidade,
+    public fatorAjuste?: FatorAjuste,
     public alrs?: BaseEntity[],
-  ) {}
+    public name?: string,
+    public sustantation?: string,
+    public der?: string,
+    public ftr?: string,
+    public grossPf?: number,
+  ) {
+    if (!pf) {
+      this.pf = 0;
+    }
+    if (!grossPf) {
+      this.grossPf = 0;
+    }
+  }
 }
