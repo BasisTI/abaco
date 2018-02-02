164a821ad4e5f06b81dc163c84948ae1854c1d2a
Diego Marcilio <diego.marcilio@basis.com.br>
2018-02-02 12:43:03 -0200

    ResumoTotal 1a impl. de um resumo total que agrega ResumoFuncoes's
    - comentario identificando duplicacao entre as classes
    -- futuro refactor

--- src/app/analise-shared/resumo-funcoes.ts
+++ src/app/analise-shared/resumo-funcoes.ts
@@ -1,4 +1,5 @@
 import { Complexidade } from './complexidade-enum';
+import { AnaliseSharedUtils } from './analise-shared-utils';
 
 export interface FuncaoResumivel {
 
@@ -10,16 +11,95 @@ export interface FuncaoResumivel {
 
 }
 
+// TODO classe abstrata comum para todos os Resumos
+// fim das contas todos os resumos tem comportamento e caracteristicas em comum
+// // total por complexidade
+// // quantidade total, totalPf, totalGrossPf
+
+export class ResumoTotal {
+
+  readonly resumosFuncoes: ResumoFuncoes[];
+
+  private _complexidadeToTotal: Map<string, number>;
+
+  private _quantidadeTotal = 0;
+  private _totalPf = 0;
+  private _totalGrossPf = 0;
+
+  private readonly complexidades: string[] = AnaliseSharedUtils.complexidades;
+
+  constructor(...resumosFuncoes: ResumoFuncoes[]) {
+    this.resumosFuncoes = resumosFuncoes;
+    this.inicializaMapaComZeroParaComplexidades();
+    resumosFuncoes.forEach(resumo => {
+      this.somaComplexidadesPorResumo(resumo);
+      this.incrementaTotais(resumo);
+    });
+  }
+
+  private inicializaMapaComZeroParaComplexidades() {
+    this.complexidades.forEach(c => this._complexidadeToTotal.set(c, 0));
+  }
+
+  private somaComplexidadesPorResumo(resumo: ResumoFuncoes) {
+    this.complexidades.forEach(complexidade => {
+      const totalDaComplexidade: number = resumo.totalPorComplexidade(Complexidade[complexidade]);
+      this.incrementaPorComplexidade(complexidade, totalDaComplexidade);
+    });
+  }
+
+  private incrementaPorComplexidade(complexidade: string, valor: number) {
+    const totalDaComplexidade = this._complexidadeToTotal.get(complexidade);
+    this._complexidadeToTotal.set(complexidade, totalDaComplexidade + valor);
+  }
+
+  private incrementaTotais(resumo: ResumoFuncoes) {
+    this._totalPf += resumo.totalPf;
+    this._totalGrossPf += resumo.totalGrossPf;
+    this._quantidadeTotal += resumo.quantidadeTotal;
+  }
+
+  totalPorComplexidade(complexidade: Complexidade): number {
+    return this._complexidadeToTotal.get(complexidade.toString());
+  }
+
+  get quantidadeTotal() {
+    return this._quantidadeTotal;
+  }
+
+  get totalPf() {
+    return this._totalPf;
+  }
+
+  get totalGrossPf() {
+    return this._totalGrossPf;
+  }
+
+}
+
 export class ResumoFuncoes {
 
   private tipoGrupoLogicoToResumo: Map<string, ResumoGrupoLogico>;
 
   private _tipos: string[];
 
+  private _complexidadeToTotal: Map<string, number>;
+
+  private _quantidadeTotal = 0;
+  private _totalPf = 0;
+  private _totalGrossPf = 0;
+
+  private readonly complexidades: string[] = AnaliseSharedUtils.complexidades;
+
   constructor(tipos: string[]) {
     this._tipos = tipos;
     this.tipoGrupoLogicoToResumo = new Map<string, ResumoGrupoLogico>();
+    this.inicializaMapaComZeroParaComplexidades();
     this.criaResumoPorGrupoLogico();
+  }
+
+  private inicializaMapaComZeroParaComplexidades() {
+    this.complexidades.forEach(c => this._complexidadeToTotal.set(c, 0));
   }
 
   private criaResumoPorGrupoLogico() {
@@ -33,10 +113,45 @@ export class ResumoFuncoes {
     const tipoGrupoLogico = funcao.tipoAsString();
     const resumo = this.tipoGrupoLogicoToResumo.get(tipoGrupoLogico);
     resumo.incrementaTotais(funcao);
+
+    this.incrementaTotais(funcao);
+  }
+
+  incrementaTotais(funcao: FuncaoResumivel) {
+    this.incrementaPorComplexidade(funcao.complexidade);
+    this.incrementaPfs(funcao.pf, funcao.grossPf);
+    this._quantidadeTotal += 1;
+  }
+
+  private incrementaPorComplexidade(complexidade: Complexidade) {
+    const complexidadeStr = complexidade;
+    const totalDaComplexidade = this._complexidadeToTotal.get(complexidadeStr);
+    this._complexidadeToTotal.set(complexidadeStr, totalDaComplexidade + 1);
+  }
+
+  private incrementaPfs(pf: number, grossPf: number) {
+    this._totalPf += pf;
+    this._totalGrossPf += grossPf;
   }
 
   get all(): ResumoGrupoLogico[] {
     return Array.from(this.tipoGrupoLogicoToResumo.values());
+  }
+
+  totalPorComplexidade(complexidade: Complexidade): number {
+    return this._complexidadeToTotal.get(complexidade.toString());
+  }
+
+  get totalPf() {
+    return this._totalPf;
+  }
+
+  get totalGrossPf() {
+    return this._totalGrossPf;
+  }
+
+  get quantidadeTotal(): number {
+    return this._quantidadeTotal;
   }
 
 }
@@ -60,9 +175,7 @@ export class ResumoGrupoLogico {
   }
 
   private inicializaOcorrenciasComoZeroParaComplexidades() {
-    // TODO extrair metodo
-    const complexidades: string[] = Object.keys(Complexidade)
-      .map(k => Complexidade[k as any]);
+    const complexidades: string[] = AnaliseSharedUtils.complexidades;
     complexidades.forEach(c => this.complexidadeToTotal.set(c, 0));
   }
 
