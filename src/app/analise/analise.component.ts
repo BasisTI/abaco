c600e722d75e0b3cb4bb612b8295eafae5f8ba15
Diego Marcilio <diego.marcilio@basis.com.br>
2018-02-06 19:17:50 -0200

    analise deletando e redirecionando para edicao

--- src/app/analise/analise.component.ts
+++ src/app/analise/analise.component.ts
@@ -6,30 +6,51 @@ import { DatatableComponent, DatatableClickEvent } from '@basis/angular-componen
 import { environment } from '../../environments/environment';
 import { Analise } from './analise.model';
 import { AnaliseService } from './analise.service';
-import { ElasticQuery } from '../shared';
-import { AfterViewInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import { ElasticQuery, PageNotificationService } from '../shared';
 
 @Component({
   selector: 'jhi-analise',
   templateUrl: './analise.component.html'
 })
-export class AnaliseComponent implements AfterViewInit {
+export class AnaliseComponent {
 
   @ViewChild(DatatableComponent) datatable: DatatableComponent;
 
   searchUrl: string = this.analiseService.searchUrl;
 
-  paginationParams = { contentIndex: null };
   elasticQuery: ElasticQuery = new ElasticQuery();
 
   constructor(
     private router: Router,
     private analiseService: AnaliseService,
-    private confirmationService: ConfirmationService
+    private confirmationService: ConfirmationService,
+    private pageNotificationService: PageNotificationService,
   ) { }
 
-  ngAfterViewInit() {
-    this.datatable.refresh(this.elasticQuery.query);
+  datatableClick(event: DatatableClickEvent) {
+    if (!event.selection) {
+      return;
+    }
+    switch (event.button) {
+      case 'edit':
+        this.router.navigate(['/analise', event.selection.id, 'edit']);
+        break;
+      case 'delete':
+        this.confirmDelete(event.selection.id);
+        break;
+    }
+  }
+
+  confirmDelete(id: any) {
+    this.confirmationService.confirm({
+      message: 'Tem certeza que deseja excluir o registro?',
+      accept: () => {
+        this.analiseService.delete(id).subscribe(() => {
+          this.datatable.refresh(this.elasticQuery.query);
+          this.pageNotificationService.addDeleteMsg();
+        });
+      }
+    });
   }
 
 }
