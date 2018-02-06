13c0d3eefd0f918fed4f5094f0078108fa349e67
felipe <felipe.souza@basis.com.br>
2018-02-06 11:12:28 -0200

    Tentativa de conserto do file upload em ambiente de produção

--- src/main/java/br/com/basis/abaco/web/rest/UploadController.java
+++ src/main/java/br/com/basis/abaco/web/rest/UploadController.java
@@ -38,9 +38,8 @@ import io.github.jhipster.web.util.ResponseUtil;
 @RestController
 @RequestMapping("/api")
 public class UploadController {
-	
-	
-    //Save the uploaded file to this folder
+
+    // Save the uploaded file to this folder
     private static String UPLOADED_FOLDER = "/uploaded";
 
     private final Logger log = LoggerFactory.getLogger(UploadController.class);
@@ -50,29 +49,32 @@ public class UploadController {
 
     @Autowired
     private UploadedFilesRepository filesRepository;
+    
+    @Autowired
+    ServletContext context;
 
     @PostMapping("/upload")
     public ResponseEntity<UploadedFile> singleFileUpload(@RequestParam("file") MultipartFile file,
-                                                         RedirectAttributes redirectAttributes) {
+            RedirectAttributes redirectAttributes) {
 
         UploadedFile uploadedFile = new UploadedFile();
         try {
             // Get the file and save it somewhere
             byte[] bytes = file.getBytes();
-            String folderPath = this.servletContext.getRealPath(UPLOADED_FOLDER);
+            String folderPath = context.getRealPath(UPLOADED_FOLDER);
             File directory = new File(folderPath);
             if (!directory.exists()) {
                 directory.mkdirs();
             }
 
-            byte[] bytesFileName = (file.getOriginalFilename() + String.valueOf(System.currentTimeMillis())).getBytes("UTF-8");
+            byte[] bytesFileName = (file.getOriginalFilename() + String.valueOf(System.currentTimeMillis()))
+                    .getBytes("UTF-8");
             String filename = DatatypeConverter.printHexBinary(MessageDigest.getInstance("MD5").digest(bytesFileName));
             String ext = FilenameUtils.getExtension(file.getOriginalFilename());
             filename += "." + ext;
             Path path = Paths.get(folderPath + "/" + filename);
             System.out.println(path);
             Files.write(path, bytes);
-
 
             uploadedFile.setDateOf(new Date());
             uploadedFile.setOriginalName(file.getOriginalFilename());
@@ -89,25 +91,26 @@ public class UploadController {
     public String uploadStatus() {
         return "uploadStatus";
     }
-    
+
     @GetMapping("/getFile")
     public ResponseEntity<Resource> getUploadedFile(@RequestParam Long id) throws IOException {
-		
-    	UploadedFile uploadedFile = filesRepository.findOne(id);
-    	
-    	String folderPath = this.servletContext.getRealPath(UPLOADED_FOLDER);
-    	
-    	Resource file = new FileSystemResource(folderPath + "/" + uploadedFile.getFilename());
-    			
-    	return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"").body(file);
-    	
-    }
-    
-    @GetMapping("/getFile/info")
-    public UploadedFile getFileInfo(@RequestParam Long id) {
-    	UploadedFile uploadedFile = filesRepository.findOne(id);
-    	
-    	return uploadedFile;
+
+        UploadedFile uploadedFile = filesRepository.findOne(id);
+
+        String folderPath = this.servletContext.getRealPath(UPLOADED_FOLDER);
+
+        Resource file = new FileSystemResource(folderPath + "/" + uploadedFile.getFilename());
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"")
+                .body(file);
+
     }
 
+    @GetMapping("/getFile/info")
+    public UploadedFile getFileInfo(@RequestParam Long id) {
+        UploadedFile uploadedFile = filesRepository.findOne(id);
+        
+        return uploadedFile;
+    }
 }
