1f1efd1809e819fc722413060b2e43da84451ebb
Diego Marcilio <diego.marcilio@basis.com.br>
2018-02-08 12:19:19 -0200

    funcao-dados cancelar de edicao com mensagem de informacao

--- src/app/funcao-dados/funcao-dados-form.component.ts
+++ src/app/funcao-dados/funcao-dados-form.component.ts
@@ -221,6 +221,7 @@ export class FuncaoDadosFormComponent implements OnInit, OnDestroy {
     this.analiseSharedDataService.funcaoAnaliseDescarregada();
     this.isEdit = false;
     this.currentFuncaoDados = new FuncaoDados();
+    this.pageNotificationService.addInfoMsg('Alteração de Função de Dados cancelada');
     this.scrollParaInicioDaAba();
   }
 
