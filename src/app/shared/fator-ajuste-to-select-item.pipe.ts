77e43eb884d3313d6c11e90bc9f52c03a6429d5b
Diego Marcilio <diego.marcilio@basis.com.br>
2018-02-08 15:20:01 -0200

    FatorAjusteLabelGenerator extracao da logica de label para fator ajuste

--- src/app/shared/fator-ajuste-to-select-item.pipe.ts
+++ src/app/shared/fator-ajuste-to-select-item.pipe.ts
@@ -1,6 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { SelectItem } from 'primeng/primeng';
 import { FatorAjuste, TipoFatorAjuste } from '../fator-ajuste';
+import { FatorAjusteLabelGenerator } from './fator-ajuste-label-generator';
 
 @Pipe({ name: 'fatorAjusteToSelectItem' })
 export class FatorAjusteToSelectItemPipe implements PipeTransform {
@@ -11,41 +12,8 @@ export class FatorAjusteToSelectItemPipe implements PipeTransform {
     }
 
     return fatoresAjuste.map(fa => {
-      const label = this.generateLabel(fa);
+      const label = FatorAjusteLabelGenerator.generate(fa);
       return { label: label, value: fa };
     });
   }
-
-  private generateLabel(fa: FatorAjuste): string {
-    const prefix = this.generatePrefix(fa);
-    const fatorSuffix = this.generateFatorSuffix(fa);
-    return `${prefix} ${fa.nome} - ${fa.fatorVisualizavel()}${fatorSuffix}`;
-  }
-
-  private generatePrefix(fa: FatorAjuste): string {
-    let prefix = '';
-    if (fa.codigo) {
-      prefix = prefix.concat(`${fa.codigo} `);
-    }
-    if (fa.origem) {
-      prefix = prefix.concat(`(${fa.origem}) `);
-    }
-    if (!this.isEmptyString(prefix)) {
-      prefix = prefix.concat('- ');
-    }
-    return prefix;
-  }
-
-  private isEmptyString(s: string) {
-    return (!s || 0 === s.length);
-  }
-
-  private generateFatorSuffix(fa: FatorAjuste): string {
-    if (fa.isPercentual()) {
-      return '%';
-    } else if (fa.isUnitario()) {
-      return ' PF';
-    }
-  }
-
 }
