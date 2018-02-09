dec232cd1eb4f3abbff295592e6a5a2f7169ae44
felipe <felipe.souza@basis.com.br>
2018-02-09 14:52:42 -0200

    BASIS-31516 - Arrumando visualização de manual

--- src/app/manual/manual-detail.component.ts
+++ src/app/manual/manual-detail.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs/Rx';
 
 import { Manual } from './manual.model';
 import { ManualService } from './manual.service';
+import { UploadService } from '../upload/upload.service';
 
 @Component({
   selector: 'jhi-manual-detail',
@@ -13,10 +14,13 @@ export class ManualDetailComponent implements OnInit, OnDestroy {
 
   manual: Manual;
   private subscription: Subscription;
+  fileName: string;
+
 
   constructor(
     private manualService: ManualService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private uploadService: UploadService
   ) {}
 
   ngOnInit() {
@@ -28,10 +32,20 @@ export class ManualDetailComponent implements OnInit, OnDestroy {
   load(id) {
     this.manualService.find(id).subscribe((manual) => {
       this.manual = manual;
+      this.getFileInfo();
     });
   }
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
+  }
+
+  getFileInfo() {
+    let fileInfo;
+    this.uploadService.getFileInfo(this.manual.arquivoManualId).subscribe(response => {
+      fileInfo = response;
+
+      this.fileName = fileInfo["originalName"];
+    });
   }
 }
