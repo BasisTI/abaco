c3db29b4a648a0c11b2d5557e1b020fc4fa7bebd
Diego Marcilio <diego.marcilio@basis.com.br>
2018-02-15 17:23:23 -0200

    Passando testes para SE e CE

--- src/app/analise-shared/calculadora/complexidade-funcional-transacao.ts
+++ src/app/analise-shared/calculadora/complexidade-funcional-transacao.ts
@@ -7,16 +7,22 @@ export class ComplexidadeFuncionalTransacao {
     derValor: number, ftrValor: number): Complexidade {
 
     if (tipo === TipoFuncaoTransacao.EE) {
-      if (derValor < 5) {
-        return this.calcularEEPorIntervaloFTR(ftrValor,
-          Complexidade.BAIXA, Complexidade.BAIXA, Complexidade.MEDIA);
-      } else if (derValor >= 5 && derValor <= 15) {
-        return this.calcularEEPorIntervaloFTR(ftrValor,
-          Complexidade.BAIXA, Complexidade.MEDIA, Complexidade.ALTA);
-      } else if (derValor > 15) {
-        return this.calcularEEPorIntervaloFTR(ftrValor,
-          Complexidade.MEDIA, Complexidade.ALTA, Complexidade.ALTA);
-      }
+      return this.calculaEE(derValor, ftrValor);
+    } else {
+      return this.calculaSEeCE(derValor, ftrValor);
+    }
+  }
+
+  private static calculaEE(derValor: number, ftrValor: number) {
+    if (derValor < 5) {
+      return this.calcularEEPorIntervaloFTR(ftrValor,
+        Complexidade.BAIXA, Complexidade.BAIXA, Complexidade.MEDIA);
+    } else if (derValor >= 5 && derValor <= 15) {
+      return this.calcularEEPorIntervaloFTR(ftrValor,
+        Complexidade.BAIXA, Complexidade.MEDIA, Complexidade.ALTA);
+    } else if (derValor > 15) {
+      return this.calcularEEPorIntervaloFTR(ftrValor,
+        Complexidade.MEDIA, Complexidade.ALTA, Complexidade.ALTA);
     }
   }
 
@@ -28,6 +34,31 @@ export class ComplexidadeFuncionalTransacao {
     } else if (ftrValor === 2) {
       return c2;
     } else if (ftrValor > 2) {
+      return c3;
+    }
+  }
+
+  private static calculaSEeCE(derValor: number, ftrValor: number) {
+    if (derValor < 6) {
+      return this.calcularSEeCEPorIntervaloFTR(ftrValor,
+        Complexidade.BAIXA, Complexidade.BAIXA, Complexidade.MEDIA);
+    } else if (derValor >= 6 && derValor <= 19) {
+      return this.calcularSEeCEPorIntervaloFTR(ftrValor,
+        Complexidade.BAIXA, Complexidade.MEDIA, Complexidade.ALTA);
+    } else if (derValor > 19) {
+      return this.calcularSEeCEPorIntervaloFTR(ftrValor,
+        Complexidade.MEDIA, Complexidade.ALTA, Complexidade.ALTA);
+    }
+  }
+
+  private static calcularSEeCEPorIntervaloFTR(ftrValor: number,
+    c1: Complexidade, c2: Complexidade, c3: Complexidade) {
+
+    if (ftrValor < 2) {
+      return c1;
+    } else if (ftrValor === 2 || ftrValor === 3) {
+      return c2;
+    } else if (ftrValor > 3) {
       return c3;
     }
   }
