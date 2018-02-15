6f53712383ffa41d845939a25d2ff5d2b4282aa6
Diego Marcilio <diego.marcilio@basis.com.br>
2018-02-15 13:59:33 -0200

    Passando testes para complexidade funcional de Funcao de Dados

--- src/app/analise-shared/calculadora/complexidade-funcional-dados.ts
+++ src/app/analise-shared/calculadora/complexidade-funcional-dados.ts
@@ -5,19 +5,25 @@ export class ComplexidadeFuncionalDados {
   static calcular(derValor: number, rlrValor: number): Complexidade {
     if (derValor < 20) {
       return this.calcularPorIntervaloRLR(rlrValor,
-         Complexidade.BAIXA, Complexidade.BAIXA, Complexidade.MEDIA);
+        Complexidade.BAIXA, Complexidade.BAIXA, Complexidade.MEDIA);
+    } else if (derValor >= 20 && derValor <= 50) {
+      return this.calcularPorIntervaloRLR(rlrValor,
+        Complexidade.BAIXA, Complexidade.MEDIA, Complexidade.ALTA);
+    } else if (derValor > 50) {
+      return this.calcularPorIntervaloRLR(rlrValor,
+        Complexidade.MEDIA, Complexidade.ALTA, Complexidade.ALTA);
     }
   }
 
   private static calcularPorIntervaloRLR(rlrValor: number, complexidade1: Complexidade,
     complexidade2: Complexidade, complexidade3: Complexidade) {
-      if (rlrValor === 1) {
-        return complexidade1;
-      } else if (rlrValor >= 2 && rlrValor <= 5) {
-        return complexidade2;
-      } else if (rlrValor >= 6) {
-        return complexidade3;
-      }
+    if (rlrValor === 1) {
+      return complexidade1;
+    } else if (rlrValor >= 2 && rlrValor <= 5) {
+      return complexidade2;
+    } else if (rlrValor > 5) {
+      return complexidade3;
+    }
   }
 
 }
