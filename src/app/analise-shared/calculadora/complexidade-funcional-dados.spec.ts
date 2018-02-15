9169e642499cd22b814367a24f3f01a3825de39c
Diego Marcilio <diego.marcilio@basis.com.br>
2018-02-15 13:30:22 -0200

    complexidade-funcional-dados.spec refactor para facilitar outros testes

--- src/app/analise-shared/calculadora/complexidade-funcional-dados.spec.ts
+++ src/app/analise-shared/calculadora/complexidade-funcional-dados.spec.ts
@@ -3,21 +3,31 @@ import { Complexidade } from '../complexidade-enum';
 
 fdescribe('Complexidade Funcional de função de Dados', () => {
 
-  let derValues: number[] = [];
   let complexidadeRetornada: Complexidade;
 
-  describe('DER para todos valores menor que 20', () => {
+  describe('DER - para todos valores menor que 20', () => {
 
-    derValues = [];
+    const derValues: number[] = [];
     for (let i = 1; i < 20; i++) {
       derValues.push(i);
     }
 
+    testesEmComum(derValues,
+      Complexidade.BAIXA,
+      Complexidade.BAIXA,
+      Complexidade.MEDIA
+    );
+  });
+
+  function testesEmComum(ders: number[],
+    complexidade1: Complexidade, complexidade2: Complexidade,
+    complexidade3: Complexidade) {
+
     describe('RLR igual a 1', () => {
-      it(`complexidade deve ser BAIXA`, () => {
-        derValues.forEach(derValue => {
+      it(`complexidade deve ser ${complexidade1}`, () => {
+        ders.forEach(derValue => {
           complexidadeRetornada = ComplexidadeFuncionalDados.calcular(derValue, 1);
-          expect(complexidadeRetornada).toEqual(Complexidade.BAIXA);
+          expect(complexidadeRetornada).toEqual(complexidade1);
         });
       });
     });
@@ -31,11 +41,11 @@ fdescribe('Complexidade Funcional de função de Dados', () => {
         }
       });
 
-      it(`complexidade deve ser BAIXA para todos RLR`, () => {
-        derValues.forEach(derValue => {
+      it(`complexidade deve ser ${complexidade2} para todos RLR`, () => {
+        ders.forEach(derValue => {
           rlrValues.forEach(rlrValue => {
             complexidadeRetornada = ComplexidadeFuncionalDados.calcular(derValue, rlrValue);
-            expect(complexidadeRetornada).toEqual(Complexidade.BAIXA);
+            expect(complexidadeRetornada).toEqual(complexidade2);
           });
         });
       });
@@ -50,17 +60,17 @@ fdescribe('Complexidade Funcional de função de Dados', () => {
       rlrValues.sort((a, b) => a - b);
 
       rlrValues.forEach(rlrValue => {
-        it(`complexidade deve ser MEDIA para RLR ${rlrValue}`, () => {
-          derValues.forEach(derValue => {
+        it(`complexidade deve ser ${complexidade3} para RLR ${rlrValue}`, () => {
+          ders.forEach(derValue => {
             complexidadeRetornada = ComplexidadeFuncionalDados.calcular(derValue, rlrValue);
-            expect(complexidadeRetornada).toEqual(Complexidade.MEDIA);
+            expect(complexidadeRetornada).toEqual(complexidade3);
           });
         });
       });
 
     });
-  });
 
+  }
 });
 
 function randomIntFromInterval(min, max): number {
