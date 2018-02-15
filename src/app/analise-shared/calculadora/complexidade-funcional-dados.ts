762654d151ff3a2676f21dc4787cb5cf74c6fab8
Diego Marcilio <diego.marcilio@basis.com.br>
2018-02-15 13:06:49 -0200

    Complexidade Funcional para FDados DER < 20

--- src/app/analise-shared/calculadora/complexidade-funcional-dados.ts
+++ src/app/analise-shared/calculadora/complexidade-funcional-dados.ts
@@ -3,7 +3,15 @@ import { Complexidade } from '../complexidade-enum';
 export class ComplexidadeFuncionalDados {
 
   static calcular(derValor: number, rlrValor: number): Complexidade {
-    
+    if (derValor < 20) {
+      if (rlrValor === 1) {
+        return Complexidade.BAIXA;
+      } else if (rlrValor >= 2 && rlrValor <= 5) {
+        return Complexidade.BAIXA;
+      } else if (rlrValor >= 6) {
+        return Complexidade.MEDIA;
+      }
+    }
   }
 
 }
