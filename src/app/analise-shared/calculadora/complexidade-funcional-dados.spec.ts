249f40a22c63358b0285c008dcd0d57d02b30607
Diego Marcilio <diego.marcilio@basis.com.br>
2018-02-15 13:06:30 -0200

    Complexidade Funcional - Agrupando testes para melhor leitura
    - melhor leitura
    - pior output dos testes em si
    -- caso algum teste em especifico falhe, vai ser horrivel debugar

--- src/app/analise-shared/calculadora/complexidade-funcional-dados.spec.ts
+++ src/app/analise-shared/calculadora/complexidade-funcional-dados.spec.ts
@@ -4,62 +4,65 @@ import { Complexidade } from '../complexidade-enum';
 fdescribe('Complexidade Funcional de função de Dados', () => {
 
   let derValues: number[] = [];
-  let rlrValues: number[] = [];
   let complexidadeRetornada: Complexidade;
 
-  describe('DER menor que 20', () => {
+  describe('DER para todos valores menor que 20', () => {
 
     derValues = [];
     for (let i = 1; i < 20; i++) {
       derValues.push(i);
     }
 
-    derValues.forEach(derValue => {
-      describe(`DER igual a ${derValue}`, () => {
-        describe('RLR igual a 1', () => {
-          it(`complexidade deve ser BAIXA`, () => {
-            complexidadeRetornada = ComplexidadeFuncionalDados.calcular(derValue, 1);
-            expect(complexidadeRetornada).toEqual(Complexidade.BAIXA);
-          });
-        });
-
-        describe('RLR maior igual a 2 ou menor igual a 5', () => {
-
-          rlrValues = [];
-          for (let i = 2; i <= 5; i++) {
-            rlrValues.push(i);
-          }
-
-          rlrValues.forEach(rlrValue => {
-            it(`complexidade deve ser BAIXA para RLR ${rlrValue}`, () => {
-              complexidadeRetornada = ComplexidadeFuncionalDados.calcular(derValue, rlrValue);
-              expect(complexidadeRetornada).toEqual(Complexidade.BAIXA);
-            });
-          });
-        });
-
-        describe('RLR maior que 5', () => {
-
-          rlrValues = [6];
-          for (let i = 0; i < 10; i++) {
-            rlrValues.push(randomIntFromInterval(6, 300));
-          }
-
-          rlrValues.forEach(rlrValue => {
-            it(`complexidade deve ser MEDIA para RLR ${rlrValue}`, () => {
-              complexidadeRetornada = ComplexidadeFuncionalDados.calcular(derValue, rlrValue);
-              expect(complexidadeRetornada).toEqual(Complexidade.MEDIA);
-            });
-          });
-
+    describe('RLR igual a 1', () => {
+      it(`complexidade deve ser BAIXA`, () => {
+        derValues.forEach(derValue => {
+          complexidadeRetornada = ComplexidadeFuncionalDados.calcular(derValue, 1);
+          expect(complexidadeRetornada).toEqual(Complexidade.BAIXA);
         });
       });
     });
 
+    describe('RLR maior igual a 2 ou menor igual a 5', () => {
+
+      const rlrValues = [];
+      beforeAll(() => {
+        for (let i = 2; i <= 5; i++) {
+          rlrValues.push(i);
+        }
+      });
+
+      it(`complexidade deve ser BAIXA para todos RLR`, () => {
+        derValues.forEach(derValue => {
+          rlrValues.forEach(rlrValue => {
+            complexidadeRetornada = ComplexidadeFuncionalDados.calcular(derValue, rlrValue);
+            expect(complexidadeRetornada).toEqual(Complexidade.BAIXA);
+          });
+        });
+      });
+    });
+
+    describe('RLR maior que 5', () => {
+
+      const rlrValues = [6];
+      for (let i = 0; i < 5; i++) {
+        rlrValues.push(randomIntFromInterval(6, 300));
+      }
+      rlrValues.sort((a, b) => a - b);
+
+      rlrValues.forEach(rlrValue => {
+        it(`complexidade deve ser MEDIA para RLR ${rlrValue}`, () => {
+          derValues.forEach(derValue => {
+            complexidadeRetornada = ComplexidadeFuncionalDados.calcular(derValue, rlrValue);
+            expect(complexidadeRetornada).toEqual(Complexidade.MEDIA);
+          });
+        });
+      });
+
+    });
   });
 
 });
 
-function randomIntFromInterval(min, max) {
+function randomIntFromInterval(min, max): number {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
