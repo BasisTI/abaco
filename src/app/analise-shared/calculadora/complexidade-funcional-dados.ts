88e8135231e7ed747018f7a8dba4f0f896315336
Diego Marcilio <diego.marcilio@basis.com.br>
2018-02-20 18:08:56 -0300

    ComplexidadeFuncionalDados metodos isXLinha() isXColuna() para reuso
    - Permitira reuso nos testes e no codigo
    - Talvez no futuro uma solucao polimorfica

--- src/app/analise-shared/calculadora/complexidade-funcional-dados.ts
+++ src/app/analise-shared/calculadora/complexidade-funcional-dados.ts
@@ -3,27 +3,51 @@ import { Complexidade } from '../complexidade-enum';
 export class ComplexidadeFuncionalDados {
 
   static calcular(derValor: number, rlrValor: number): Complexidade {
-    if (derValor < 20) {
+    if (this.isPrimeiraColuna(derValor)) {
       return this.calcularPorIntervaloRLR(rlrValor,
         Complexidade.BAIXA, Complexidade.BAIXA, Complexidade.MEDIA);
-    } else if (derValor >= 20 && derValor <= 50) {
+    } else if (this.isSegundaColuna(derValor)) {
       return this.calcularPorIntervaloRLR(rlrValor,
         Complexidade.BAIXA, Complexidade.MEDIA, Complexidade.ALTA);
-    } else if (derValor > 50) {
+    } else if (this.isTerceiraColuna(derValor)) {
       return this.calcularPorIntervaloRLR(rlrValor,
         Complexidade.MEDIA, Complexidade.ALTA, Complexidade.ALTA);
     }
   }
 
+  static isPrimeiraColuna(der: number): boolean {
+    return der < 20;
+  }
+
+  static isSegundaColuna(der: number): boolean {
+    return der >= 20 && der <= 50;
+  }
+
+  static isTerceiraColuna(der: number): boolean {
+    return der > 50;
+  }
+
   private static calcularPorIntervaloRLR(rlrValor: number, complexidade1: Complexidade,
     complexidade2: Complexidade, complexidade3: Complexidade) {
-    if (rlrValor === 1) {
+    if (this.isPrimeiraLinha(rlrValor)) {
       return complexidade1;
-    } else if (rlrValor >= 2 && rlrValor <= 5) {
+    } else if (this.isSegundaLinha(rlrValor)) {
       return complexidade2;
-    } else if (rlrValor > 5) {
+    } else if (this.isTerceiraLinha(rlrValor)) {
       return complexidade3;
     }
+  }
+
+  static isPrimeiraLinha(rlr: number) {
+    return rlr === 1;
+  }
+
+  static isSegundaLinha(rlr: number) {
+    return rlr >= 2 && rlr <= 5;
+  }
+
+  static isTerceiraLinha(rlr: number) {
+    return rlr > 5;
   }
 
 }
