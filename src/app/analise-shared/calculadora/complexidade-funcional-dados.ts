f15781225bb9868eecc58df16973bc3ccaba79a2
Diego Marcilio <diego.marcilio@basis.com.br>
2018-02-21 08:14:43 -0300

    Removendo metodos estaticos isXColuna() isXLinha() das complexidades funcionais

--- src/app/analise-shared/calculadora/complexidade-funcional-dados.ts
+++ src/app/analise-shared/calculadora/complexidade-funcional-dados.ts
@@ -7,28 +7,4 @@ export class ComplexidadeFuncionalDados {
     return new ComplexidadeFuncionalALIeAIE(derValor, rlrValor).calcular();
   }
 
-  static isPrimeiraColuna(der: number): boolean {
-    return der < 20;
-  }
-
-  static isSegundaColuna(der: number): boolean {
-    return der >= 20 && der <= 50;
-  }
-
-  static isTerceiraColuna(der: number): boolean {
-    return der > 50;
-  }
-
-  static isPrimeiraLinha(rlr: number) {
-    return rlr === 1;
-  }
-
-  static isSegundaLinha(rlr: number) {
-    return rlr >= 2 && rlr <= 5;
-  }
-
-  static isTerceiraLinha(rlr: number) {
-    return rlr > 5;
-  }
-
 }
