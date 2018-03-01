7ab412bf69cb279ce467eedcaddcd9991c90e52d
Diego Marcilio <diego.marcilio@basis.com.br>
2018-03-01 19:00:27 -0300

    FuncaoDadosRepository com metodos provaveis

--- src/main/java/br/com/basis/abaco/repository/FuncaoDadosRepository.java
+++ src/main/java/br/com/basis/abaco/repository/FuncaoDadosRepository.java
@@ -1,12 +1,24 @@
 package br.com.basis.abaco.repository;
 
-import br.com.basis.abaco.domain.FuncaoDados;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.basis.abaco.domain.FuncaoDados;
+import br.com.basis.abaco.domain.FuncaoDadosVersionavel;
 
 /**
  * Spring Data JPA repository for the FuncaoDados entity.
  */
-@SuppressWarnings("unused")
 public interface FuncaoDadosRepository extends JpaRepository<FuncaoDados, Long> {
+
+    Optional<FuncaoDados> findFirstByFuncaoDadosVersionavelOrderByAuditUpdatedOnDesc(
+            FuncaoDadosVersionavel funcaoDadosVersionavel);
+
+    Optional<FuncaoDados> findFirstByFuncaoDadosVersionavelIdOrderByAuditUpdatedOnDesc(
+            FuncaoDadosVersionavel funcaoDadosVersionavel);
+
+    List<FuncaoDados> findByFuncaoDadosVersionavelIn(List<FuncaoDadosVersionavel> funcoesDadosVersionaveis);
 
 }
