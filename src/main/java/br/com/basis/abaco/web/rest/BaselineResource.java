212633986b99aaedbfac98646bcab659663dad4b
Eduardo Monteiro de Andrade <eduardo.andrade@basis.com.br>
2018-03-26 08:59:56 -0300

    inicio da baseline

---
+++ src/main/java/br/com/basis/abaco/web/rest/BaselineResource.java
@@ -0,0 +1,17 @@
+package br.com.basis.abaco.web.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @author eduardo.andrade
+ * @since 20/03/2018
+ */
+@RestController
+@RequestMapping("/baseline")
+public class BaselineResource {
+	
+	
+
+}
