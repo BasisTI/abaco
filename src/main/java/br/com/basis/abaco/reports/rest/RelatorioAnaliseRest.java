04f1550e957c15bb009d27713c5b5386ff8a5070
Eduardo Monteiro de Andrade <eduardo.andrade@basis.com.br>
2018-05-07 14:32:04 -0300

    ajustes para correção dos erros no sonar

--- src/main/java/br/com/basis/abaco/reports/rest/RelatorioAnaliseRest.java
+++ src/main/java/br/com/basis/abaco/reports/rest/RelatorioAnaliseRest.java
@@ -22,118 +22,118 @@ import net.sf.jasperreports.engine.JRException;
 @Path("/relatorio")
 public class RelatorioAnaliseRest {
 
-	private static String caminhoRalatorioAnalise = "reports/analise/analise.jasper";
+    private static String caminhoRalatorioAnalise = "reports/analise/analise.jasper";
 
-	@Context
-	HttpServletRequest request;
+    @Context
+    HttpServletRequest request;
 
-	private Analise analise;
+    private Analise analise;
 
-	private Map<String, Object> parametro;
+    private Map<String, Object> parametro;
 
-	private void init() {
-		analise = new Analise();
-	}
+    private void init() {
+        analise = new Analise();
+    }
 
-	@GET
-	@Path("/pdfAnalise/{idAnalise}")
-	public Response downloadAnalise(@PathParam("idAnalise") Long idAnalise) throws IOException, JRException {
-		init();
-		RelatorioUtil relatorio = new RelatorioUtil();
-		byte[] pdf = relatorio.gerarPdf(request, caminhoRalatorioAnalise, popularParametroAnalise());
-		ResponseBuilder response = Response.ok((Object) pdf);
-		response.header("Content-Disposition", "attachment; Analise " + analise.getIdentificadorAnalise() + ".pdf");
-		return response.build();
+    @GET
+    @Path("/pdfAnalise/{idAnalise}")
+    public Response downloadAnalise(@PathParam("idAnalise") Long idAnalise) throws IOException, JRException {
+        init();
+        RelatorioUtil relatorio = new RelatorioUtil();
+        byte[] pdf = relatorio.gerarPdf(request, caminhoRalatorioAnalise, popularParametroAnalise());
+        ResponseBuilder response = Response.ok((Object) pdf);
+        response.header("Content-Disposition", "attachment; Analise " + analise.getIdentificadorAnalise() + ".pdf");
+        return response.build();
 
-	}
+    }
 
-	/**
-	 * Método responsável por popular o parametro do Jasper.
-	 */
-	private Map<String, Object> popularParametroAnalise() {
-		parametro = new HashMap<String, Object>();
+    /**
+     * Método responsável por popular o parametro do Jasper.
+     */
+    private Map<String, Object> popularParametroAnalise() {
+        parametro = new HashMap<String, Object>();
+        
+        parametro.put("PFTOTAL", analise.getPfTotal());
+        parametro.put("PFAJUSTADO", analise.getAdjustPFTotal());
+        parametro.put("DATACRIADO", analise.getAudit().getCreatedOn());
+        parametro.put("DATAALTERADO", analise.getAudit().getUpdatedOn());
+        parametro.put("CRIADOPOR", analise.getCreatedBy());
+        parametro.put("EDITADOPOR", analise.getEditedBy());
+        parametro.put("VALORAJUSTE", analise.getValorAjuste());
+        parametro.put("GATANTIA", garantia());
+        
+        popularDadosBasicos();
+        popularContrato();
+        popularSistema();
+        popularManual();
+        
+        return parametro;
+    }
 
-		parametro.put("PFTOTAL", analise.getPfTotal());
-		parametro.put("PFAJUSTADO", analise.getAdjustPFTotal());
-		parametro.put("DATACRIADO", analise.getAudit().getCreatedOn());
-		parametro.put("DATAALTERADO", analise.getAudit().getUpdatedOn());
-		parametro.put("CRIADOPOR", analise.getCreatedBy());
-		parametro.put("EDITADOPOR", analise.getEditedBy());
-		parametro.put("VALORAJUSTE", analise.getValorAjuste());
-		parametro.put("GATANTIA", garantia());
+    private void popularDadosBasicos() {
+        parametro.put("EQUIPE", analise.getEquipeResponsavel());
+        parametro.put("IDENTIFICADOR", analise.getIdentificadorAnalise());
+        parametro.put("FATORAJUSTE", analise.getFatorAjuste().getNome());
+        parametro.put("TIPOANALISE", analise.getTipoAnalise());
+        parametro.put("PROPOSITO", validarNulos(analise.getPropositoContagem()));
+        parametro.put("ESCOPO", validarNulos(analise.getEscopo()));
+        parametro.put("FONTEIRA", analise.getFronteiras());
+        parametro.put("DOCUMENTACAO", analise.getDocumentacao());
+        parametro.put("FUNCAODADOS", analise.getFuncaoDados());
+        parametro.put("FUNCAOTRANSACAO", analise.getFuncaoTransacaos());
+        parametro.put("AJUSTES", analise.getFatorAjuste());
+        parametro.put("OBSERVACOES", analise.getObservacoes());
+        parametro.put("DATAHOMOLOGACAO", analise.getDataHomologacao());
+        parametro.put("NUMEROOS", analise.getNumeroOs());
+    }
 
-		popularDadosBasicos();
-		popularContrato();
-		popularSistema();
-		popularManual();
+    private void popularContrato() {
 
-		return parametro;
-	}
-	
-	private void popularDadosBasicos() {
-		parametro.put("EQUIPE", analise.getEquipeResponsavel());
-		parametro.put("IDENTIFICADOR", analise.getIdentificadorAnalise());
-		parametro.put("FATORAJUSTE", analise.getFatorAjuste().getNome());
-		parametro.put("TIPOANALISE", analise.getTipoAnalise());
-		parametro.put("PROPOSITO", validarNulos(analise.getPropositoContagem()));
-		parametro.put("ESCOPO", validarNulos(analise.getEscopo()));
-		parametro.put("FONTEIRA", analise.getFronteiras());
-		parametro.put("DOCUMENTACAO", analise.getDocumentacao());
-		parametro.put("FUNCAODADOS", analise.getFuncaoDados());
-		parametro.put("FUNCAOTRANSACAO", analise.getFuncaoTransacaos());
-		parametro.put("AJUSTES", analise.getFatorAjuste());
-		parametro.put("OBSERVACOES", analise.getObservacoes());
-		parametro.put("DATAHOMOLOGACAO", analise.getDataHomologacao());
-		parametro.put("NUMEROOS", analise.getNumeroOs());
-	}
+        if (analise.getContrato() != null) {
+            parametro.put("CONTRATO", analise.getContrato().getNumeroContrato());
+            parametro.put("ORGANIZACAO", analise.getContrato().getOrganization().getSigla());
+            parametro.put("ORGANIZACAONM", analise.getContrato().getOrganization().getNome());
+            parametro.put("CONTRATODTINICIO", analise.getContrato().getDataInicioVigencia());
+            parametro.put("CONTRATODTFIM", analise.getContrato().getDataFimVigencia());
+            parametro.put("CONTRATOGARANTIA", analise.getContrato().getDiasDeGarantia());
+            parametro.put("CONTRATOATIVO", analise.getContrato().getAtivo());
+            parametro.put("VERSAOCPM", analise.getContrato().getManual().getVersaoCPM());
+        }
 
-	private void popularContrato() {
+    }
 
-		if (analise.getContrato() != null) {
-			parametro.put("CONTRATO", analise.getContrato().getNumeroContrato());
-			parametro.put("ORGANIZACAO", analise.getContrato().getOrganization().getSigla());
-			parametro.put("ORGANIZACAONM", analise.getContrato().getOrganization().getNome());
-			parametro.put("CONTRATODTINICIO", analise.getContrato().getDataInicioVigencia());
-			parametro.put("CONTRATODTFIM", analise.getContrato().getDataFimVigencia());
-			parametro.put("CONTRATOGARANTIA", analise.getContrato().getDiasDeGarantia());
-			parametro.put("CONTRATOATIVO", analise.getContrato().getAtivo());
-			parametro.put("VERSAOCPM", analise.getContrato().getManual().getVersaoCPM());
-		}
+    private void popularSistema() {
 
-	}
+        if (analise.getSistema() != null) {
+            parametro.put("SISTEMASG", analise.getSistema().getSigla());
+            parametro.put("SISTEMANM", analise.getSistema().getNome());
+            parametro.put("SISTEMAMODULOS", analise.getSistema().getModulos());
+        }
 
-	private void popularSistema() {
+    }
 
-		if (analise.getSistema() != null) {
-			parametro.put("SISTEMASG", analise.getSistema().getSigla());
-			parametro.put("SISTEMANM", analise.getSistema().getNome());
-			parametro.put("SISTEMAMODULOS", analise.getSistema().getModulos());
-		}
+    private void popularManual() {
 
-	}
+        if (analise.getContrato() != null && analise.getContrato().getManual() != null) {
+            parametro.put("MANUALNM", analise.getContrato().getManual().getNome());
+            parametro.put("METODOCONTAGEM", analise.getMetodoContagem());
+        }
+    }
 
-	private void popularManual() {
+    private String garantia() {
+        if (analise.getBaselineImediatamente()) {
+            return "Sim";
+        } else {
+            return "Não";
+        }
+    }
 
-		if (analise.getContrato() != null && analise.getContrato().getManual() != null) {
-			parametro.put("MANUALNM", analise.getContrato().getManual().getNome());
-			parametro.put("METODOCONTAGEM", analise.getMetodoContagem());
-		}
-	}
-
-	private String garantia() {
-		if (analise.getBaselineImediatamente()) {
-			return "Sim";
-		} else {
-			return "Não";
-		}
-	}
-
-	private String validarNulos(String valor) {
-		if (valor == null) {
-			return "---";
-		} else {
-			return valor;
-		}
-	}
+    private String validarNulos(String valor) {
+        if (valor == null) {
+            return "---";
+        } else {
+            return valor;
+        }
+    }
 
 }
