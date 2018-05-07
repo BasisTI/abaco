8e8ce59929f3865595ce804695cf1ad1ae40e7ea
Eduardo Monteiro de Andrade <eduardo.andrade@basis.com.br>
2018-05-07 10:49:12 -0300

    inclusão dos dependências no pom

--- src/main/java/br/com/basis/abaco/reports/rest/RelatorioAnaliseRest.java
+++ src/main/java/br/com/basis/abaco/reports/rest/RelatorioAnaliseRest.java
@@ -1,7 +1,6 @@
 package br.com.basis.abaco.reports.rest;
 
 import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,12 +22,15 @@ import net.sf.jasperreports.engine.JRException;
 @Path("/relatorio")
 public class RelatorioAnaliseRest {
 	
-	private static String caminhoRalatorioCertificado = "/relatorios/Analise.jasper";
-
+	private static String caminhoRalatorioAnalise = "reports/analise/analise.jasper";
+//	private static String caminhoImagem = "reports/analise/imagem/fnde_mec_brasil.png";
+	
     @Context
     HttpServletRequest request;
     
 	private Analise analise;
+	
+	private Map<String, Object> parametro;
     
     private void init() {
     	analise = new Analise(); 
@@ -41,7 +43,7 @@ public class RelatorioAnaliseRest {
 		
 		RelatorioUtil relatorio = new RelatorioUtil();
 		
-		byte[] pdf = relatorio.gerarPdf(request, caminhoRalatorioCertificado, popularParametroAnalise());
+		byte[] pdf = relatorio.gerarPdf(request, caminhoRalatorioAnalise, popularParametroAnalise());
 		
 		ResponseBuilder response = Response.ok((Object) pdf);
 		
@@ -50,29 +52,98 @@ public class RelatorioAnaliseRest {
 
 	}
 	
-	private SimpleDateFormat formatarData() {
-		SimpleDateFormat dataFormatada = new SimpleDateFormat("dd/MM/yyyy");
-		
-		return dataFormatada;
-	}
+//	private SimpleDateFormat formatarData() {
+//		SimpleDateFormat dataFormatada = new SimpleDateFormat("dd/MM/yyyy");
+//		
+//		return dataFormatada;
+//	}
 	
     /**
      * Método responsável por popular o parametro do Jasper.
      */   
     private Map<String, Object> popularParametroAnalise() {
-    	Map<String, Object> parametro = new HashMap<String, Object>();
+    	parametro = new HashMap<String, Object>();
     	
-    	parametro.put("PROPOSITO", analise.getPropositoContagem());
-    	parametro.put("ESCOPO", analise.getEscopo());
+    	parametro.put("PROPOSITO", validarNulos(analise.getPropositoContagem()));
+    	parametro.put("ESCOPO", validarNulos(analise.getEscopo()));
     	parametro.put("FONTEIRA", analise.getFronteiras());
-    	parametro.put("METODO_CONTAGEM", analise.getContrato().getManual().getVersaoCPM());
     	parametro.put("DOCUMENTACAO", analise.getDocumentacao());
-    	parametro.put("FUNCAO_DADOS", analise.getFuncaoDados());
-    	parametro.put("FUNCAO_TRANSACAO", analise.getFuncaoTransacaos());
+    	parametro.put("FUNCAODADOS", analise.getFuncaoDados());
+    	parametro.put("FUNCAOTRANSACAO", analise.getFuncaoTransacaos());
     	parametro.put("AJUSTES", analise.getFatorAjuste());
     	parametro.put("OBSERVACOES", analise.getObservacoes());
+
+    	parametro.put("PFTOTAL", analise.getPfTotal());
+    	parametro.put("PFAJUSTADO", analise.getAdjustPFTotal());
+    	parametro.put("TIPOANALISE", analise.getTipoAnalise());
+    	
+    	parametro.put("DATACRIADO", analise.getAudit().getCreatedOn());
+    	parametro.put("DATAALTERADO", analise.getAudit().getUpdatedOn());
+    	parametro.put("CRIADOPOR", analise.getCreatedBy());
+    	parametro.put("EDITADOPOR", analise.getEditedBy());
+    	parametro.put("DATAHOMOLOGACAO", analise.getDataHomologacao());
+    	parametro.put("EQUIPE", analise.getEquipeResponsavel());
+    	parametro.put("NUMEROOS", analise.getNumeroOs());
+    	parametro.put("VALORAJUSTE", analise.getValorAjuste());
+    	parametro.put("IDENTIFICADOR", analise.getIdentificadorAnalise());
+    	parametro.put("GATANTIA", garantia());
+    	parametro.put("FATORAJUSTE", analise.getFatorAjuste().getNome());
+    	
+    	popularContrato();
+    	popularSistema();
+    	popularManual();
     	
     	return parametro;
+    }
+    
+    private void popularContrato() {
+    	
+    	if(analise.getContrato() != null) {
+        	parametro.put("CONTRATO", analise.getContrato().getNumeroContrato());
+        	parametro.put("ORGANIZACAO", analise.getContrato().getOrganization().getSigla());
+        	parametro.put("ORGANIZACAONM", analise.getContrato().getOrganization().getNome());
+        	parametro.put("CONTRATODTINICIO", analise.getContrato().getDataInicioVigencia());
+        	parametro.put("CONTRATODTFIM", analise.getContrato().getDataFimVigencia());
+        	parametro.put("CONTRATOGARANTIA", analise.getContrato().getDiasDeGarantia());
+        	parametro.put("CONTRATOATIVO", analise.getContrato().getAtivo());
+        	parametro.put("VERSAOCPM", analise.getContrato().getManual().getVersaoCPM());
+    	}
+    	
+    }
+    
+    private void popularSistema() {
+
+    	if(analise.getSistema() != null) {
+    		parametro.put("SISTEMASG", analise.getSistema().getSigla());
+    		parametro.put("SISTEMANM", analise.getSistema().getNome());
+    		parametro.put("SISTEMAMODULOS", analise.getSistema().getModulos());
+    	}
+    	
+    	
+    }
+    
+    private void popularManual() {
+    	
+    	if(analise.getContrato() != null && analise.getContrato().getManual() != null) {
+    		parametro.put("MANUALNM", analise.getContrato().getManual().getNome());
+    		parametro.put("METODOCONTAGEM", analise.getMetodoContagem());
+    	}
+    }
+    
+    private String garantia() {
+    	if(analise.getBaselineImediatamente()) {
+    		return "Sim";
+    	} else {
+    		return "Não";
+    	}
+    }
+    
+    private String validarNulos(String valor) {
+    	if(valor == null) {
+    		return "---";
+    	} else {
+    		return valor;
+    	}
     }
     
     
