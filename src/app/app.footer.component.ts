9721ab9dddc64ccbc78b0ca5724dbf259d1fec02
Eduardo Monteiro de Andrade <eduardo.andrade@basis.com.br>
2018-05-28 10:34:38 -0300

    ajustando a responsividade das telas de pesquisa.

--- src/app/app.footer.component.ts
+++ src/app/app.footer.component.ts
@@ -7,19 +7,27 @@ import { AppComponent } from './app.component';
     <div class="footer">
         <div class="card clearfix">
 
-          <span class="footer-text-left">
-            Desenvolvido por Basis Tecnologia S.A. <a HREF="http://www.basis.com.br" TARGET="_blank">basis.com.br</a>
-          </span>
+          <div class="ui-g-6 ui-md-6 ui-sm-12">
+            <span class="footer-text-left">
+              Desenvolvido por Basis Tecnologia S.A. <a HREF="http://www.basis.com.br" TARGET="_blank">basis.com.br</a>
+            </span>
+          </div>
 
-          <span class="footer-text-right">
-            <span class="ui-icon ui-icon-copyright"></span>
-            <span>
-              <a HREF="https://www.basis.com.br/nossos-produtos" TARGET="_blank">Clique aqui para ver a licença.</a>
+          <div class="ui-g-6 ui-md-6 ui-sm-12">
+            <span class="footer-text-right">
+
+              <span class="ui-icon ui-icon-copyright"></span>
+
+                <span>
+                  <a HREF="https://www.basis.com.br/nossos-produtos" TARGET="_blank">Clique aqui para ver a licença.</a>
+                </span>
+
+                <span>
+                  <app-version-tag class="footer-text-left"></app-version-tag>
+                </span>
+
             </span>
-            <span>
-              <app-version-tag class="footer-text-left"></app-version-tag>
-            </span>
-          </span>
+          </div>
 
         </div>
     </div>
