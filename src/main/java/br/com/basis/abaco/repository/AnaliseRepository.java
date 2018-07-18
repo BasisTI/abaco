edc44b170a15a518dc60e699416f095c982577ed
Brayan Quartezani Costa <brayan.costa@basis.com.br>
2018-07-18 16:41:35 -0300

    Ajustes Análise

--- src/main/java/br/com/basis/abaco/repository/AnaliseRepository.java
+++ src/main/java/br/com/basis/abaco/repository/AnaliseRepository.java
@@ -3,10 +3,12 @@ package br.com.basis.abaco.repository;
 import br.com.basis.abaco.domain.Analise;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 /**
  * Spring Data JPA repository for the Analise entity.
  */
 @SuppressWarnings("unused")
 public interface AnaliseRepository extends JpaRepository<Analise,Long> {
-    <Optional> Analise findOneById (Long id);
+    Optional<Analise> findOneById (Long id);
 }
