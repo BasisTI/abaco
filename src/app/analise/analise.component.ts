dd5a08aca06c816d2df73031ea49eacba0de2f2a
william-fernandes <william-fernandes@basis.com.br>
2018-08-07 15:26:06 -0300

    Responsividade da tabela de análise Correçõe de estrutura do código em análise

--- src/app/analise/analise.component.ts
+++ src/app/analise/analise.component.ts
@@ -1,246 +1,254 @@
-import { Component, ViewChild, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { ConfirmationService} from 'primeng/primeng';
-import { DatatableComponent, DatatableClickEvent } from '@basis/angular-components';
-import { BlockUI, NgBlockUI} from 'ng-block-ui';
-import { environment } from '../../environments/environment';
-import { Analise } from './analise.model';
-import { AnaliseService } from './analise.service';
-import { ElasticQuery, PageNotificationService } from '../shared';
-import { MessageUtil } from '../util/message.util';
-import { FuncaoDados } from '../funcao-dados';
+import {Component, ViewChild, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {ConfirmationService} from 'primeng/primeng';
+import {DatatableComponent, DatatableClickEvent} from '@basis/angular-components';
+import {BlockUI, NgBlockUI} from 'ng-block-ui';
+import {Analise} from './analise.model';
+import {AnaliseService} from './analise.service';
+import {ElasticQuery, PageNotificationService} from '../shared';
+import {MessageUtil} from '../util/message.util';
 
 @Component({
-  // tslint:disable-next-line:component-selector
-  selector: 'jhi-analise',
-  templateUrl: './analise.component.html'
+    selector: 'jhi-analise',
+    templateUrl: './analise.component.html'
 })
 export class AnaliseComponent implements OnInit {
 
-  @BlockUI() blockUI: NgBlockUI;
+    @BlockUI() blockUI: NgBlockUI;
 
-  @ViewChild(DatatableComponent) datatable: DatatableComponent;
+    @ViewChild(DatatableComponent) datatable: DatatableComponent;
 
-  searchUrl: string = this.analiseService.searchUrl;
+    searchUrl: string = this.analiseService.searchUrl;
 
-  elasticQuery: ElasticQuery = new ElasticQuery();
+    elasticQuery: ElasticQuery = new ElasticQuery();
 
-  rowsPerPageOptions: number[] = [5, 10, 20, 50, 100];
+    rowsPerPageOptions: number[] = [5, 10, 20, 50, 100];
 
-  analiseSelecionada; analiseReadyToClone: Analise;
+    analiseSelecionada;
+    analiseReadyToClone: Analise;
 
-  block: boolean;
+    block: boolean;
 
-  unblock: boolean;
+    unblock: boolean;
 
-  
-  /**
-   *
-   */
-  constructor(
-    private router: Router,
-    private analiseService: AnaliseService,
-    private confirmationService: ConfirmationService,
-    private pageNotificationService: PageNotificationService,
-  ) { }
-
-  /**
-   *
-   */
-  public ngOnInit() {
-    this.block = true;
-    this.unblock = true;
-    this.datatable.pDatatableComponent.onRowSelect.subscribe((event) => {
-      this.analiseReadyToClone = new Analise().copyFromJSON(event.data);
-      this.block = true;
-      this.unblock = true;
-      this.analiseSelecionada = event.data;
-      this.block = !event.data.bloqueiaAnalise;
-      this.unblock = event.data.bloqueiaAnalise;
-    });
-    this.datatable.pDatatableComponent.onRowUnselect.subscribe((event) => {
-      this.analiseSelecionada = undefined;
-      this.analiseReadyToClone = undefined;
-    });
-  }
-
-  /**
-   *
-   */
-  public datatableClick(event: DatatableClickEvent) {
-    if (!event.selection) {
-      return;
+    constructor(
+        private router: Router,
+        private analiseService: AnaliseService,
+        private confirmationService: ConfirmationService,
+        private pageNotificationService: PageNotificationService,
+    ) {
     }
-    switch (event.button) {
-      case 'edit':
-        if(event.selection.bloqueiaAnalise){ this.pageNotificationService.addErrorMsg('Você não pode editar uma análise bloqueada!'); return }
-        this.router.navigate(['/analise', event.selection.id, 'edit']);
-        break;
-      case 'delete':
-        this.confirmDelete(event.selection);
-        break;
-      case 'relatorioBrowser':
-        this.geraRelatorioPdfBrowser(event.selection);
-        break;
-      case 'relatorioArquivo' :
-        this.gerarRelatorioPdfArquivo(event.selection);
-        break;
-      case 'relatorioBrowserDetalhado' :
-        this.geraRelatorioPdfDetalhadoBrowser(event.selection);
-        break;
-      case 'clone' :
-        this.clonar(this.analiseReadyToClone);
-        break;
+
+    public ngOnInit() {
+        this.block = true;
+        this.unblock = true;
+        this.datatable.pDatatableComponent.onRowSelect.subscribe((event) => {
+            this.analiseReadyToClone = new Analise().copyFromJSON(event.data);
+            this.block = true;
+            this.unblock = true;
+            this.analiseSelecionada = event.data;
+            this.block = !event.data.bloqueiaAnalise;
+            this.unblock = event.data.bloqueiaAnalise;
+            console.log(event.data);
+        });
+        this.datatable.pDatatableComponent.onRowUnselect.subscribe((event) => {
+            this.analiseSelecionada = undefined;
+            this.analiseReadyToClone = undefined;
+        });
     }
-  }
 
-  public clonar(analise: Analise){
-    const analiseClonada = analise.clone();
-    this.confirmationService.confirm({
-      message: MessageUtil.CONFIRMAR_CLONE + ' ' + this.analiseSelecionada.identificadorAnalise + '?',
-      accept: () => {
-        analiseClonada.id = undefined;
-        analiseClonada.identificadorAnalise += " - CÓPIA";
-        analiseClonada.bloqueiaAnalise = false;
+    /**
+     * Clique na tabela análise
+     */
+    public datatableClick(event: DatatableClickEvent) {
+        if (!event.selection) {
+            return;
+        }
+        switch (event.button) {
+            case 'edit':
+                if (event.selection.bloqueiaAnalise) {
+                    this.pageNotificationService.addErrorMsg(
+                        MessageUtil.ERRO_EXCLUSAO_ANALISE_BLOQUEADA);
+                    return;
+                }
+                this.router.navigate(['/analise', event.selection.id, 'edit']);
+                break;
+            case 'delete':
+                this.confirmDelete(event.selection);
+                break;
+            case 'relatorioBrowser':
+                this.geraRelatorioPdfBrowser(event.selection);
+                break;
+            case 'relatorioArquivo' :
+                this.gerarRelatorioPdfArquivo(event.selection);
+                break;
+            case 'relatorioBrowserDetalhado' :
+                this.geraRelatorioPdfDetalhadoBrowser(event.selection);
+                break;
+            case 'clone' :
+                this.clonar(this.analiseReadyToClone);
+                break;
+        }
+    }
 
-        analiseClonada.funcaoDados.forEach(FuncaoDados => { 
-          FuncaoDados.id = undefined;
-          FuncaoDados.ders.forEach(Ders => {
-            Ders.id = undefined;
-          });
-          FuncaoDados.rlrs.forEach(rlrs => {
-            rlrs.id = undefined;
-          });
+    /**
+     * Clonar análise
+     */
+    public clonar(analise: Analise) {
+        const analiseClonada = analise.clone();
+        this.confirmationService.confirm({
+            message: MessageUtil.CONFIRMAR_CLONE.concat(this.analiseSelecionada.identificadorAnalise).concat('?'),
+            accept: () => {
+                analiseClonada.id = undefined;
+                analiseClonada.identificadorAnalise += MessageUtil.CONCAT_COPIA;
+                analiseClonada.bloqueiaAnalise = false;
+
+                analiseClonada.funcaoDados.forEach(FuncaoDados => {
+                    FuncaoDados.id = undefined;
+                    FuncaoDados.ders.forEach(Ders => {
+                        Ders.id = undefined;
+                    });
+                    FuncaoDados.rlrs.forEach(rlrs => {
+                        rlrs.id = undefined;
+                    });
+                });
+
+                analiseClonada.funcaoTransacaos.forEach(funcaoTransacaos => {
+                    funcaoTransacaos.id = undefined;
+                    funcaoTransacaos.ders.forEach(ders => {
+                        ders.id = undefined;
+                    });
+                    funcaoTransacaos.alrs.forEach(alrs => {
+                        alrs.id = undefined;
+                    });
+                });
+
+                this.analiseService.create(analiseClonada).subscribe((res: any) => {
+                    const menssagem: string = MessageUtil.ANALISE.concat(' ').
+                    concat(this.analiseSelecionada.identificadorAnalise).
+                    concat(MessageUtil.CLONAGEM_SUCESSO);
+
+                    this.pageNotificationService.addSuccessMsg(menssagem);
+                    this.recarregarDataTable();
+                    this.router.navigate(['/analise', res.id, 'edit']);
+                });
+            }
         });
 
-        analiseClonada.funcaoTransacaos.forEach(funcaoTransacaos => {
-          funcaoTransacaos.id = undefined;
-          funcaoTransacaos.ders.forEach(ders => {
-            ders.id = undefined;
-          });
-          funcaoTransacaos.alrs.forEach(alrs => {
-            alrs.id = undefined;
-          });
+    }
+
+    /**
+     * Confirmar deleção de uma análise
+     */
+    public confirmDelete(analise: Analise) {
+        if (this.analiseSelecionada.bloqueiaAnalise) {
+            this.pageNotificationService.addErrorMsg(MessageUtil.ERRO_EXCLUSAO_ANALISE_BLOQUEADA);
+            return;
+        }
+        this.confirmationService.confirm({
+            message: MessageUtil.CONFIRMAR_EXCLUSAO.concat(analise.identificadorAnalise).concat('?'),
+            accept: () => {
+                this.blockUI.start(MessageUtil.EXCLUINDO_REGISTRO);
+                this.analiseService.delete(analise.id).subscribe(() => {
+                    this.recarregarDataTable();
+                    this.blockUI.stop();
+                    this.pageNotificationService.addDeleteMsgWithName(analise.identificadorAnalise);
+                });
+            }
         });
+    }
 
-        this.analiseService.create(analiseClonada).subscribe((res) => {
-          
-          this.pageNotificationService.addSuccessMsg(`Análise '${this.analiseSelecionada.identificadorAnalise}' clonada com sucesso!`);
-          this.recarregarDataTable();
-          this.router.navigate(['/analise', res.id, 'edit']);
+    /**
+     * Limpa a pesquisa e recarrega a tabela
+     */
+    public limparPesquisa() {
+        this.elasticQuery.reset();
+        this.recarregarDataTable();
+    }
+
+    /**
+     * Recarrega a tabela de análise
+     */
+    public recarregarDataTable() {
+        this.datatable.refresh(this.elasticQuery.query);
+    }
+
+    /**
+     * Método responsável por gerar o relatório diretamente sem a apresentação do relatório no browser.
+     * @param analise
+     */
+    public gerarRelatorioPdfArquivo(analise: Analise) {
+        this.analiseService.gerarRelatorioPdfArquivo(analise.id);
+    }
+
+    /**
+     * Método responsável pela a apresentação do relatório no browser.
+     * @param analise
+     */
+    public geraRelatorioPdfBrowser(analise: Analise) {
+        this.analiseService.geraRelatorioPdfBrowser(analise.id);
+    }
+
+    /**
+     * Método responsável por gerar o relatório detalhado da analise.
+     * @param analise
+     */
+    public geraRelatorioPdfDetalhadoBrowser(analise: Analise) {
+        if ((analise.metodoContagem === MessageUtil.DETALHADA && (analise.funcaoDados.length === 0
+            || analise.funcaoTransacaos.length === 0)) || analise.funcaoDados.length === 0) {
+            this.pageNotificationService.addErrorMsg(MessageUtil.ERRO_PDF_RELATORIO_CAMPOS);
+        } else {
+            this.analiseService.geraRelatorioPdfDetalhadoBrowser(analise.id);
+        }
+    }
+
+    /**
+     * Desabilita botão relatório
+     */
+    public desabilitarBotaoRelatorio(): boolean {
+        return !!this.analiseSelecionada;
+    }
+
+    /**
+     * Bloqueia relatório
+     */
+    public bloqueiaRelatorio() {
+        if (this.analiseSelecionada.bloqueiaAnalise) {
+            this.pageNotificationService.addErrorMsg(MessageUtil.REGISTRO_ESTA_BLOQUEADO);
+            return;
+        }
+        this.confirmationService.confirm({
+            message: MessageUtil.CONFIRMAR_BLOQUEIO.concat(this.analiseSelecionada.identificadorAnalise).concat('?'),
+            accept: () => {
+                this.analiseService.block(this.analiseSelecionada).subscribe(() => {
+                    this.pageNotificationService.addBlockMsgWithName(this.analiseSelecionada.identificadorAnalise);
+                    this.recarregarDataTable();
+                    this.block = true;
+                    this.unblock = true;
+                });
+            }
         });
-      }
-    });
-    
-  }
-  /**
-   *
-   */
-  public confirmDelete(analise: Analise) {
-    if(this.analiseSelecionada.bloqueiaAnalise){
-      this.pageNotificationService.addErrorMsg('Você não pode excluir uma análise bloqueada!');
-      return;
     }
-    this.confirmationService.confirm({
-      message: MessageUtil.CONFIRMAR_EXCLUSAO + ' ' + analise.identificadorAnalise + '?',
-      accept: () => {
-        this.blockUI.start(MessageUtil.EXCLUINDO_REGISTRO);
-        this.analiseService.delete(analise.id).subscribe(() => {
-          this.recarregarDataTable();
-          this.blockUI.stop();
-          this.pageNotificationService.addDeleteMsgWithName(analise.identificadorAnalise);
+
+    /**
+     * Desbloquear relatório
+     */
+    public desbloqueiaRelatorio() {
+        if (!this.analiseSelecionada.bloqueiaAnalise) {
+            this.pageNotificationService.addErrorMsg(MessageUtil.REGISTRO_ESTA_BLOQUEADO);
+            return;
+        }
+        this.confirmationService.confirm({
+            message: MessageUtil.CONFIRMAR_DESBLOQUEIO.concat(' ').
+            concat(this.analiseSelecionada.identificadorAnalise).concat('?'),
+            accept: () => {
+                this.analiseService.unblock(this.analiseSelecionada).subscribe(() => {
+                    this.pageNotificationService.addUnblockMsgWithName(this.analiseSelecionada.identificadorAnalise);
+                    this.recarregarDataTable();
+                    this.block = true;
+                    this.unblock = true;
+                });
+            }
         });
-      }
-    });
-  }
-
-  /**
-   *
-   */
-  public limparPesquisa() {
-    this.elasticQuery.reset();
-    this.recarregarDataTable();
-  }
-
-  /**
-   *
-   */
-  public recarregarDataTable() {
-    this.datatable.refresh(this.elasticQuery.query);
-  }
-
-  /**
-   * Método responsável por gerar o relatório diretamente sem a apresentação do relatório no browser.
-   * @param analise
-   */
-  public gerarRelatorioPdfArquivo(analise: Analise) {
-    this.analiseService.gerarRelatorioPdfArquivo(analise.id);
-  }
-
-  /**
-   * Método responsável pela a apresentação do relatório no browser.
-   * @param analise
-   */
-  public geraRelatorioPdfBrowser(analise: Analise) {
-    this.analiseService.geraRelatorioPdfBrowser(analise.id);
-  }
-
-  /**
-   * Método responsável por gerar o relatório detalhado da analise.
-   * @param analise
-   */
-  public geraRelatorioPdfDetalhadoBrowser(analise: Analise) {
-    if ((analise.metodoContagem === "DETALHADA" && (analise.funcaoDados.length === 0 || analise.funcaoTransacaos.length === 0)) || analise.funcaoDados.length === 0){
-      this.pageNotificationService.addErrorMsg('Preencha os campos Função de dados e/ou Função de transação para gerar um relatório');
-    } else {
-      this.analiseService.geraRelatorioPdfDetalhadoBrowser(analise.id);
     }
-  }
-
-  /**
-   *
-   */
-  public desabilitarBotaoRelatorio(): boolean {
-    if (this.analiseSelecionada) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  public bloqueiaRelatorio() {
-    if(this.analiseSelecionada.bloqueiaAnalise){
-      this.pageNotificationService.addErrorMsg('Registro já está bloqueado');
-      return;
-    }
-    this.confirmationService.confirm({
-      message: MessageUtil.CONFIRMAR_BLOQUEIO + ' ' + this.analiseSelecionada.identificadorAnalise + '?',
-      accept: () => {
-        this.analiseService.block(this.analiseSelecionada).subscribe(() => {
-          this.pageNotificationService.addBlockMsgWithName(this.analiseSelecionada.identificadorAnalise);
-          this.recarregarDataTable();
-          this.block = true;
-          this.unblock = true;
-        })
-      }
-    });
-  }
-
-  public desbloqueiaRelatorio() {
-    if(!this.analiseSelecionada.bloqueiaAnalise){
-      this.pageNotificationService.addErrorMsg('Registro já está desbloqueado');
-      return;
-    }
-    this.confirmationService.confirm({
-      message: MessageUtil.CONFIRMAR_DESBLOQUEIO + ' ' + this.analiseSelecionada.identificadorAnalise + '?',
-      accept: () => {
-        this.analiseService.unblock(this.analiseSelecionada).subscribe(() => {
-          this.pageNotificationService.addUnblockMsgWithName(this.analiseSelecionada.identificadorAnalise);
-          this.recarregarDataTable();
-          this.block = true;
-          this.unblock = true;
-        });
-      }
-    });
-  }
 }
