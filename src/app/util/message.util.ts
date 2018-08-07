10536567b1ab2ced37372b708c9084971f311cb4
william-fernandes <william-fernandes@basis.com.br>
2018-08-07 15:31:43 -0300

    Responsividade da tabela de análise Correçõe de estrutura do código em análise

--- src/app/util/message.util.ts
+++ src/app/util/message.util.ts
@@ -1,11 +1,14 @@
 export class MessageUtil {
     public static SELECIONE = 'Selecione...';
-    public static CONFIRMAR_EXCLUSAO = 'Tem certeza que deseja excluiWr o registro?';
+    public static CONFIRMAR_EXCLUSAO = 'Tem certeza que deseja exclui o registro ';
     public static ERROR_DELETE_REGISTRO = 'Falha ao excluir registro';
     public static EXCLUINDO_REGISTRO = 'Excluindo...';
     public static CONFIRMAR_BLOQUEIO = 'Tem certeza que deseja bloquear o registro';
     public static CONFIRMAR_DESBLOQUEIO = 'Tem certeza que deseja desbloquear o registro';
-    public static CONFIRMAR_CLONE = 'Tem certeza que deseja clonar o registro';
+    public static CONFIRMAR_CLONE = 'Tem certeza que deseja clonar o registro ';
+    public static ANALISE = 'Análise';
+    public static ERROR_SALVAR_REGISTRO = 'Falha ao salvar registro';
+
 
     public static INDICATIVA = 'INDICATIVA';
     public static DETALHADA = 'DETALHADA';
@@ -30,4 +33,10 @@ export class MessageUtil {
     public static SELECIONE_CONTRATO_CONTINUAR = 'Informe o Contrato para continuar';
     public static INFORME_METODO_CONTAGEM = 'Informe o Método de Contagem para continuar';
     public static INFORME_TIPO_CONTAGEM = 'Informe o Tipo de Contagem para continuar';
+
+    public static CONCAT_COPIA = ' - CÓPIA';
+    public static ERRO_EXCLUSAO_ANALISE_BLOQUEADA = 'Você não pode editar uma análise bloqueada';
+    public static CLONAGEM_SUCESSO = ' clonada com sucesso!';
+    public static ERRO_PDF_RELATORIO_CAMPOS = 'Preencha os campos Funções de Dados e/ou Função de Transação para gerar um relatório';
+    public static REGISTRO_ESTA_BLOQUEADO = 'Registro já está bloqueado';
 }
