55d0dd242f401b253c8bd165b4de0eba46e26d02
fabio.brandao <fabio.brandao@basis.com.br>
2018-08-17 09:05:38 -0300

    Adicionando imports que faltaram.

--- src/main/java/br/com/basis/abaco/domain/TipoEquipe.java
+++ src/main/java/br/com/basis/abaco/domain/TipoEquipe.java
@@ -24,6 +24,9 @@ import br.com.basis.dynamicexports.pojo.ReportObject;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.annotations.FieldIndex;
 
 /**
  * A TipoEquipe.
