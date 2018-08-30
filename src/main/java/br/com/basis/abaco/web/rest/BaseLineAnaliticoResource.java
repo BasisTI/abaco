0ae77b0b50387d397e804fb390eb17c1bbd8524f
Brayan Quartezani Costa <brayan.costa@basis.com.br>
2018-08-30 15:27:08 -0300

    Correções sonar

--- src/main/java/br/com/basis/abaco/web/rest/BaseLineAnaliticoResource.java
+++ src/main/java/br/com/basis/abaco/web/rest/BaseLineAnaliticoResource.java
@@ -13,7 +13,11 @@ import net.sf.jasperreports.engine.JRException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 
 import java.util.ArrayList;
