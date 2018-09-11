151da5d24afc9413821f08a1baa3e3e12d39a0c5
eduardo.aires <eduardo.aires@basis.com.br>
2018-09-11 18:07:47 -0300

    Não salvar com mesmo nome FD e FT

--- src/app/funcao-transacao/funcao-transacao-form.component.ts
+++ src/app/funcao-transacao/funcao-transacao-form.component.ts
@@ -230,19 +230,44 @@ export class FuncaoTransacaoFormComponent implements OnInit, OnDestroy {
         if (!retorno) {
             this.pageNotificationService.addErrorMsg('Favor preencher o campo obrigatório!');
             return;
-        }
+        } else {
         this.desconverterChips();
         this.verificarModulo();
         const funcaoTransacaoCalculada = CalculadoraTransacao.calcular(
             this.analise.metodoContagem, this.currentFuncaoTransacao, this.analise.contrato.manual);
 
-        this.analise.addFuncaoTransacao(funcaoTransacaoCalculada);
-        this.atualizaResumo();
-        this.resetarEstadoPosSalvar();
+            this.validarNameFuncaoTransacaos(this.currentFuncaoTransacao.name).then( resolve => {
+                if (resolve){
+                    this.analise.addFuncaoTransacao(funcaoTransacaoCalculada);
+                    this.atualizaResumo();
+                    this.resetarEstadoPosSalvar();
+                    this.salvarAnalise();
+                    this.fecharDialog();
+                    this.pageNotificationService.addCreateMsgWithName(funcaoTransacaoCalculada.name);
+                    this.atualizaResumo();
+                    this.resetarEstadoPosSalvar();
+                } else {
+                    this.pageNotificationService.addErrorMsg('Registro já cadastrado!');
+                }
+             });
+    }
+}
 
-        this.salvarAnalise();
-        this.fecharDialog();
-        this.pageNotificationService.addCreateMsgWithName(funcaoTransacaoCalculada.name);
+    validarNameFuncaoTransacaos(nome: string) {
+        const that = this;
+        return new Promise( resolve => {
+            if (that.analise.funcaoTransacaos.length === 0) {
+                return resolve(true);
+            }
+            that.analise.funcaoTransacaos.forEach( (data, index) => {
+                if (data.name === nome) {
+                    return resolve(false);
+                }
+                if (!that.analise.funcaoTransacaos[index + 1]){
+                    return resolve(true);
+                }
+            });
+        });
     }
 
     private verifyDataRequire(): boolean {
@@ -330,22 +355,26 @@ export class FuncaoTransacaoFormComponent implements OnInit, OnDestroy {
         if (!retorno) {
             this.pageNotificationService.addErrorMsg('Favor preencher o campo obrigatório!');
             return;
-        }
+        } else {
         this.desconverterChips();
         this.verificarModulo();
-
         const funcaoTransacaoCalculada = CalculadoraTransacao.calcular(
-            this.analise.metodoContagem, this.currentFuncaoTransacao, this.analise.contrato.manual
-        );
-
-        this.analise.updateFuncaoTransacao(funcaoTransacaoCalculada);
-        this.atualizaResumo();
-        this.resetarEstadoPosSalvar();
-
-        this.salvarAnalise();
-        this.fecharDialog();
-        this.pageNotificationService.addSuccessMsg(`Função de Transação '${funcaoTransacaoCalculada.name}' alterada com sucesso`);
-
+            this.analise.metodoContagem, this.currentFuncaoTransacao, this.analise.contrato.manual);
+            this.validarNameFuncaoTransacaos(this.currentFuncaoTransacao.name).then( resolve => {
+                if (resolve){
+                    this.analise.updateFuncaoTransacao(funcaoTransacaoCalculada);
+                    this.atualizaResumo();
+                    this.resetarEstadoPosSalvar();
+                    this.salvarAnalise();
+                    this.fecharDialog();
+                    this.pageNotificationService.addSuccessMsg(`Função de Transação '${funcaoTransacaoCalculada.name}' alterada com sucesso`);
+                    this.atualizaResumo();
+                    this.resetarEstadoPosSalvar();
+                } else {
+                    this.pageNotificationService.addErrorMsg('Registro já cadastrado!');
+                }
+             });
+        }
     }
 
     fecharDialog() {
