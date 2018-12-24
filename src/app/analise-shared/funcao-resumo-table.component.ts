1c52db8b8411b4aa6b012687bd2ce3ff31b0904f
jpmartins <joao.correa@basis.com.br>
2018-12-24 11:31:21 -0200

    Corrigindo erros da tela 'Função de dados' e 'Resumo'

--- src/app/analise-shared/funcao-resumo-table.component.ts
+++ src/app/analise-shared/funcao-resumo-table.component.ts
@@ -32,12 +32,8 @@ export class FuncaoResumoTableComponent implements OnInit {
         return 'INM';
       case 'BAIXA':
         return 'BAIXA';
-<<<<<<< HEAD
-      break;
-
-
-=======
->>>>>>> 42c6bfda4467a102236aa90c0076834daaab6edc
+      default:
+        break;
       }
   }
 
@@ -51,12 +47,9 @@ export class FuncaoResumoTableComponent implements OnInit {
         return 'EXCLUSÃO';
       case 'CONVERSAO' :
         return 'CONVERSÃO';
-<<<<<<< HEAD
-      break;
-=======
-      //break;
+      default:
+        break;
 
->>>>>>> 42c6bfda4467a102236aa90c0076834daaab6edc
       }
   }
 
