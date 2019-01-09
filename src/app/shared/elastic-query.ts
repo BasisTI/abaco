0a4cd26832d77a69a5b961d544eb329bd4ea8d61
filipe.freitas <filipe.freitas@basis.com.br>
2019-01-09 12:38:52 -0200

    Correção pesquisa CNPJ

--- src/app/shared/elastic-query.ts
+++ src/app/shared/elastic-query.ts
@@ -2,7 +2,7 @@ export class ElasticQuery {
 
     private static readonly wildcard = '*';
 
-    value: string;
+    value: string = "";
 
     get query(): string {
         return this.value ? `${this.value}`  : ElasticQuery.wildcard;
