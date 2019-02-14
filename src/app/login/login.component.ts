ef180e67ffb5ab98dd8477aae2bb1d5068e8069b
Rafael Borges <rafael.borges@basis.com.br>
2019-02-14 18:13:03 -0200

    Nova Página de Login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,6 +11,7 @@ import { PageNotificationService } from '../shared/page-notification.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit, OnDestroy {
 
