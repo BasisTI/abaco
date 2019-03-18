1f77b19a8d2613e33b68fd6742162f0e7b56788c
Chrysthian Moizes <chrysthianmoizes@gmail.com>
2019-03-18 09:14:45 -0300

    correção de importação para Sonar

--- src/main/java/br/com/basis/abaco/domain/ManualContrato.java
+++ src/main/java/br/com/basis/abaco/domain/ManualContrato.java
@@ -4,7 +4,16 @@ import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.Objects;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
 import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
