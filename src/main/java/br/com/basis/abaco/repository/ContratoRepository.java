ed686a58b0c345017213b8f1ad733dd136b1d9de
DavyGCLima <davy.g.c.lima@gmail.com>
2019-03-25 15:56:08 -0300

    BASIS-76481 correção da duplicação de contratos em  editar análise, correção do Sonar

--- src/main/java/br/com/basis/abaco/repository/ContratoRepository.java
+++ src/main/java/br/com/basis/abaco/repository/ContratoRepository.java
@@ -1,8 +1,6 @@
 package br.com.basis.abaco.repository;
 
 import br.com.basis.abaco.domain.Contrato;
-import br.com.basis.abaco.domain.Organizacao;
-
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
