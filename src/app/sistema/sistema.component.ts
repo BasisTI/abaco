d26dfe916e33c2b6826c33cd9e75217bd24f23c4
Rai <www.rai12@hotmail.com>
2019-05-02 10:18:11 -0300

    BASIS-78640 Remoções de console.log

--- src/app/sistema/sistema.component.ts
+++ src/app/sistema/sistema.component.ts
@@ -291,8 +291,6 @@ export class SistemaComponent implements AfterViewInit {
      *
      */
     public recarregarDataTable() {
-        console.log(this.datatable)
-        console.log(this.elasticQuery.query)
         this.datatable.url = this.searchUrl;
         this.datatable.refresh(this.elasticQuery.value ? this.elasticQuery.value : this.elasticQuery.query);
     }
