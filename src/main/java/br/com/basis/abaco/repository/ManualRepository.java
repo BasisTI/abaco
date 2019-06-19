e7a2b21b24adace130473bcd036b67a7a74506fe
DavyGCLima <davy.g.c.lima@gmail.com>
2019-06-19 15:45:37 -0300

    COreções de repositórios para não usarem native queries (Sonar)

--- src/main/java/br/com/basis/abaco/repository/ManualRepository.java
+++ src/main/java/br/com/basis/abaco/repository/ManualRepository.java
@@ -3,7 +3,6 @@ package br.com.basis.abaco.repository;
 import br.com.basis.abaco.domain.Manual;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
--- src/main/java/br/com/basis/abaco/repository/OrganizacaoRepository.java
+++ src/main/java/br/com/basis/abaco/repository/OrganizacaoRepository.java
@@ -4,7 +4,6 @@ import br.com.basis.abaco.domain.Organizacao;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
