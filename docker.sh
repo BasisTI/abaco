9ce13a93d7e2a16ecce01958c7779157fabbbfa5
raniere.lacerda <raniere.lacerda@basis.com.br>
2019-06-27 14:41:40 -0300

    Remoção de código desnecessário e criação de dockerfile - BASIS-85143

--- docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Subindo os containers do docker!"
-
-sudo docker-compose -f ./src/main/docker/postgresql.yml up -d
-sudo docker-compose -f ./src/main/docker/jhipster-registry.yml up -d
-sudo docker-compose -f ./src/main/docker/elasticsearch.yml up -d
-
-echo "Ready!"
