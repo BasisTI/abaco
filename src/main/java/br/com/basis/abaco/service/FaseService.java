fc24404edb36e202ba590fb95ce4f6b89f380ca1
DavyGCLima <davy.g.c.lima@gmail.com>
2019-08-14 12:02:29 -0300

    BASIS-85548 FiltroFaseDTO, sonar

--- src/main/java/br/com/basis/abaco/service/FaseService.java
+++ src/main/java/br/com/basis/abaco/service/FaseService.java
@@ -20,8 +20,6 @@ import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
-import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
