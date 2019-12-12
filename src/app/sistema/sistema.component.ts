c4c600b8003bcbc7f8586979ae84ba030cbb9f3d
edivaldo.melo <edivaldo.melo@basis.com.br>
2019-12-12 17:45:09 -0300

    BASIS-94955 Pesquisar sistema - Não pesquisa por parte do nome

--- src/app/sistema/sistema.component.ts
+++ src/app/sistema/sistema.component.ts
@@ -2,7 +2,6 @@ import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ConfirmationService } from 'primeng/primeng';
 import { DatatableComponent, DatatableClickEvent } from '@basis/angular-components';
-import { environment } from '../../environments/environment';
 import { Sistema } from './sistema.model';
 import { SistemaService } from './sistema.service';
 import { ElasticQuery, PageNotificationService } from '../shared';
@@ -10,8 +9,6 @@ import { Organizacao } from '../organizacao/organizacao.model';
 import { OrganizacaoService } from '../organizacao/organizacao.service';
 import { StringConcatService } from '../shared/string-concat.service';
 import { BlockUI, NgBlockUI } from 'ng-block-ui';
-import { MessageUtil } from '../util/message.util';
-import { Response } from '@angular/http';
 import { TranslateService } from '@ngx-translate/core';
 
 
@@ -105,15 +102,9 @@ export class SistemaComponent {
             this.abrirEditar();
         }
     }
-
     abrirEditar() {
         this.router.navigate(['/sistema', this.sistemaSelecionado.id, 'edit']);
     }
-
-    /**
-     *
-     * @param id
-     */
     public confirmDelete(id: any) {
         this.confirmationService.confirm({
             message: this.getLabel('Global.Mensagens.CertezaExcluirRegistro'),
@@ -123,151 +114,78 @@ export class SistemaComponent {
                     this.limparPesquisa();
                     this.pageNotificationService.addDeleteMsg();
                     this.blockUI.stop();
-                }, (error: Response) => {
+                }, (error) => {
                     if (error.headers.toJSON()['x-abacoapp-error'][0] === 'error.analiseexists') {
-                        this.pageNotificationService.addErrorMsg(this.getLabel('Cadastros.Sistema.Mensagens.msgSistemaVinculadoNaoPodeSerExcluido'));
+                        this.pageNotificationService.addErrorMsg(
+                            this.getLabel('Cadastros.Sistema.Mensagens.msgSistemaVinculadoNaoPodeSerExcluido')
+                        );
                     }
                 }
                 );
             }
         });
     }
-
-    public switchUrlSigla() {
-
-        if (((this.searchParams.sigla === undefined) || (this.searchParams.sigla !== '')) &&
-            ((this.searchParams.nomeSistema === undefined) || (this.searchParams.nomeSistema === '')) &&
-            ((this.searchParams.organizacao.nome === undefined) || (this.searchParams.organizacao.nome === ''))) {
-
-            this.searchUrl = this.sistemaService.fieldSearchSiglaUrl;
-        }
-
-    }
-
-    public switchUrlNomeSistema() {
-        if (((this.searchParams.nomeSistema === undefined) || (this.searchParams.nomeSistema !== '')) &&
-            ((this.searchParams.sigla === undefined) || (this.searchParams.sigla === '')) &&
-            ((this.searchParams.organizacao.nome === undefined) || (this.searchParams.organizacao.nome === ''))) {
-
-            this.searchUrl = this.sistemaService.fieldSearchSistemaUrl;
-
-        }
-
-    }
-
-    public switchUrlOrganizacao() {
-
-        if (((this.searchParams.organizacao.nome === undefined) || (this.searchParams.organizacao.nome !== '')) &&
-            ((this.searchParams.nomeSistema === undefined) || (this.searchParams.nomeSistema === '')) &&
-            ((this.searchParams.sigla === undefined) || (this.searchParams.sigla === ''))) {
-
-            this.searchUrl = this.sistemaService.fieldSearchOrganizacaoUrl;
-
-        }
-
-    }
-
-    public switchSearchUrl() {
-
-        if ((this.searchParams.sigla !== undefined) && (this.searchParams.nomeSistema !== undefined) && (this.searchParams.organizacao.nome !== undefined)) {
-            this.searchUrl = this.sistemaService.searchUrl;
-        }
-        else if ((this.searchParams.nomeSistema !== undefined) && (this.searchParams.sigla !== undefined) && (this.searchParams.organizacao.nome === undefined)) {
-            this.searchUrl = this.sistemaService.searchUrl;
-        }
-        else if ((this.searchParams.organizacao.nome !== undefined) && (this.searchParams.sigla !== undefined) && (this.searchParams.nomeSistema === undefined)) {
-            this.searchUrl = this.sistemaService.searchUrl;
-        }
-        else if ((this.searchParams.organizacao.nome !== undefined) && (this.searchParams.nomeSistema !== undefined) && (this.searchParams.sigla === undefined)) {
-            this.searchUrl = this.sistemaService.searchUrl;
-        }
-
-    }
-
-    /**
-     *
-     */
     private checkUndefinedParams() {
         (this.searchParams.sigla === '') ? (this.searchParams.sigla = undefined) : (this);
         (this.searchParams.nomeSistema === '') ? (this.searchParams.nomeSistema = undefined) : (this);
         (this.searchParams.organizacao.nome === '') ? (this.searchParams.organizacao.nome = undefined) : (this);
     }
-
-    /**
-     *
-     */
     public performSearch() {
-        this.checkUrl();
         this.checkUndefinedParams();
         this.elasticQuery.value = this.stringConcatService.concatResults(this.createStringParamsArray());
         this.recarregarDataTable();
 
     }
-
-    checkUrl() {
-        this.switchSearchUrl();
-        this.switchUrlSigla();
-        this.switchUrlNomeSistema();
-        this.switchUrlOrganizacao();
-    }
-
-    /**
-     *
-     */
     private createStringParamsArray(): Array<string> {
-        let stringParamsArray: Array<string> = [];
+        const stringParamsArray: Array<string> = [];
 
-        if (this.searchParams.sigla !== undefined && this.searchParams.nomeSistema === undefined && this.searchParams.organizacao.nome === undefined) {
-            (this.searchParams.sigla !== undefined || this.elasticQuery.value == '') ? (stringParamsArray.push("*" + this.searchParams.sigla + "*")) : (this);
-        }
-        else if (this.searchParams.nomeSistema !== undefined && this.searchParams.sigla === undefined && this.searchParams.organizacao.nome === undefined) {
-            (this.searchParams.nomeSistema !== undefined || this.elasticQuery.value == '') ? (stringParamsArray.push("*" + this.searchParams.nomeSistema + "*")) : (this);
-        }
-        else if (this.searchParams.organizacao.nome !== undefined && this.searchParams.nomeSistema === undefined && this.searchParams.sigla === undefined) {
-            (this.searchParams.organizacao.nome !== undefined || this.elasticQuery.value == '') ? (stringParamsArray.push("*" + this.searchParams.organizacao.nome + "*")) : (this);
-        }
-        else if (this.searchParams.sigla === undefined && this.searchParams.nomeSistema !== undefined && this.searchParams.organizacao.nome !== undefined) {
-            (this.searchParams.nomeSistema !== undefined || this.elasticQuery.value == '') ? (stringParamsArray.push('nome:' + this.searchParams.nomeSistema)) : (this);
-            (this.searchParams.organizacao.nome !== undefined || this.elasticQuery.value == '') ? (stringParamsArray.push("AND organizacao.nome:" + this.searchParams.organizacao.nome)) : (this);
-        }
-        else if (this.searchParams.nomeSistema === undefined && this.searchParams.sigla !== undefined && this.searchParams.organizacao.nome !== undefined) {
-            (this.searchParams.sigla !== undefined || this.elasticQuery.value == '') ? (stringParamsArray.push("sigla:" + this.searchParams.sigla)) : (this);
-            (this.searchParams.organizacao.nome !== undefined || this.elasticQuery.value == '') ? (stringParamsArray.push("AND organizacao.nome:" + this.searchParams.organizacao.nome)) : (this);
-        }
-        else if (this.searchParams.organizacao.nome === undefined && this.searchParams.sigla !== undefined && this.searchParams.nomeSistema !== undefined) {
-            (this.searchParams.sigla !== undefined || this.elasticQuery.value == '') ? (stringParamsArray.push("sigla:" + this.searchParams.sigla)) : (this);
-            (this.searchParams.nomeSistema !== undefined || this.elasticQuery.value != '') ? (stringParamsArray.push('AND nome:' + this.searchParams.nomeSistema)) : (this);
-        }
-        else if (this.searchParams.sigla !== undefined && this.searchParams.nomeSistema !== undefined && this.searchParams.organizacao.nome !== undefined) {
-            (this.searchParams.sigla !== undefined || this.elasticQuery.value == '') ? (stringParamsArray.push("sigla:" + this.searchParams.sigla)) : (this);
-            (this.searchParams.nomeSistema !== undefined || this.elasticQuery.value == '') ? (stringParamsArray.push('AND nome:' + this.searchParams.nomeSistema)) : (this);
-            (this.searchParams.organizacao.nome !== undefined || this.elasticQuery.value == '') ? (stringParamsArray.push("AND organizacao.nome:" + this.searchParams.organizacao.nome)) : (this);
+        if (this.searchParams.sigla !== undefined && this.searchParams.sigla !== '' &&
+            this.searchParams.nomeSistema === undefined &&
+            this.searchParams.organizacao.nome === undefined) {
+            (this.searchParams.sigla !== undefined || this.elasticQuery.value === '' ) ?
+                    (stringParamsArray.push('*' + this.searchParams.sigla + '*')) : (this);
+        }else if (this.searchParams.nomeSistema !== undefined &&
+            this.searchParams.sigla === undefined && this.searchParams.organizacao.nome === undefined) {
+                (this.searchParams.nomeSistema !== undefined || this.elasticQuery.value === '') ? (
+                    stringParamsArray.push('*' + this.searchParams.nomeSistema + '*')) : (this);
+        }else if (this.searchParams.organizacao.nome !== undefined &&
+            this.searchParams.nomeSistema === undefined && this.searchParams.sigla === undefined) {
+            (this.searchParams.organizacao.nome !== undefined ||
+                this.elasticQuery.value === '') ? (
+                    stringParamsArray.push('*' + this.searchParams.organizacao.nome + '*')) : (this);
+        }else if (this.searchParams.sigla === undefined &&
+            this.searchParams.nomeSistema !== undefined &&
+            this.searchParams.organizacao.nome !== undefined) {
+            (this.searchParams.nomeSistema !== undefined || this.elasticQuery.value === '') ? (
+                stringParamsArray.push('nome: *' + this.searchParams.nomeSistema + '*')) : (this);
+            (this.searchParams.organizacao.nome !== undefined || this.elasticQuery.value === '') ? (
+                stringParamsArray.push('AND organizacao.nome:' + this.searchParams.organizacao.nome)) : (this);
+        }else if (this.searchParams.nomeSistema === undefined &&
+            this.searchParams.sigla !== undefined &&
+            this.searchParams.organizacao.nome !== undefined) {
+            (this.searchParams.sigla !== undefined || this.elasticQuery.value === '') ?
+                (stringParamsArray.push('sigla:*' + this.searchParams.sigla + '*')) : (this);
+            (this.searchParams.organizacao.nome !== undefined || this.elasticQuery.value === '') ?
+                (stringParamsArray.push('AND organizacao.nome:' + this.searchParams.organizacao.nome)) : (this);
+        }else if (this.searchParams.organizacao.nome === undefined &&
+            this.searchParams.sigla !== undefined &&
+            this.searchParams.nomeSistema !== undefined) {
+            (this.searchParams.sigla !== undefined || this.elasticQuery.value === '') ?
+                (stringParamsArray.push('sigla:*' + this.searchParams.sigla + '*')) : (this);
+            (this.searchParams.nomeSistema !== undefined || this.elasticQuery.value !== '') ?
+                (stringParamsArray.push('AND nome: *' + this.searchParams.nomeSistema + '*')) : (this);
+        }else if (this.searchParams.sigla !== undefined &&
+            this.searchParams.nomeSistema !== undefined &&
+            this.searchParams.organizacao.nome !== undefined) {
+            (this.searchParams.sigla !== undefined || this.elasticQuery.value === '') ?
+                (stringParamsArray.push('sigla:*' + this.searchParams.sigla + '*')) : (this);
+            (this.searchParams.nomeSistema !== undefined || this.elasticQuery.value === '') ?
+                (stringParamsArray.push('AND nome: *' + this.searchParams.nomeSistema + '*')) : (this);
+            (this.searchParams.organizacao.nome !== undefined || this.elasticQuery.value === '') ?
+                (stringParamsArray.push('AND organizacao.nome:' + this.searchParams.organizacao.nome)) : (this);
         }
         return stringParamsArray;
     }
-
-    /**
-     *
-     */
-    private checkFields(): void {
-
-        if ((this.searchParams.nomeSistema === undefined) && ((this.searchParams.organizacao.nome === undefined) || (this.searchParams.organizacao.nome === ''))) {
-            this.searchUrl = this.sistemaService.fieldSearchSiglaUrl;
-
-        } else if ((this.searchParams.sigla === undefined) && ((this.searchParams.organizacao.nome === undefined) || (this.searchParams.organizacao.nome === ''))) {
-            this.searchUrl = this.sistemaService.fieldSearchSistemaUrl;
-
-        } else if ((this.searchParams.sigla === undefined) && (this.searchParams.nomeSistema === undefined)) {
-            this.searchParams.organizacao.nome = this.sistemaService.searchUrl;
-        }
-
-        this.recarregarDataTable();
-
-    }
-
-    /**
-     *
-     */
     public limparPesquisa() {
         this.searchParams.sigla = '';
         this.searchParams.organizacao = '';
@@ -276,10 +194,6 @@ export class SistemaComponent {
         this.searchUrl = this.sistemaService.searchUrl;
         this.recarregarDataTable();
     }
-
-    /**
-     *
-     */
     public recarregarDataTable() {
         this.datatable.url = this.searchUrl;
         this.datatable.refresh(this.elasticQuery.value ? this.elasticQuery.value : this.elasticQuery.query);
