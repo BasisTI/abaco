26fe0fb6eb1362ba85442797ed3f8e31e59ee91a
edivaldo.melo <edivaldo.melo@basis.com.br>
2020-02-12 10:06:10 -0300

    BASIS-102181 - fix bug security block request

--- src/main/java/br/com/basis/abaco/web/rest/PEAnaliticoResource.java
+++ src/main/java/br/com/basis/abaco/web/rest/PEAnaliticoResource.java
@@ -3,11 +3,13 @@ package br.com.basis.abaco.web.rest;
 import br.com.basis.abaco.domain.PEAnalitico;
 import br.com.basis.abaco.repository.FuncaoTransacaoRepository;
 import br.com.basis.abaco.repository.PEAnaliticoRepository;
+import br.com.basis.abaco.security.AuthoritiesConstants;
 import br.com.basis.abaco.service.dto.DropdownDTO;
 import com.codahale.metrics.annotation.Timed;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,6 +41,7 @@ public class PEAnaliticoResource {
 
     @GetMapping("/peanalitico/drop-down/{id}")
     @Timed
+    @Secured({AuthoritiesConstants.ADMIN, AuthoritiesConstants.USER, AuthoritiesConstants.GESTOR, AuthoritiesConstants.ANALISTA})
     public Set<DropdownDTO> getFuncaoDadosByAnalise(@PathVariable Long id) {
         Set<PEAnalitico> peAnaliticos = peAnaliticoRepository.findAllByidsistema(id);
         return peAnaliticos.stream().map(this::convertToDto).collect(Collectors.toSet());
