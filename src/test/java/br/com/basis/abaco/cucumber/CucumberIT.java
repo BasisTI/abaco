ce1c61cf9a020d85207fbf736e6d16c4bd13bd93
edivaldo.melo <edivaldo.melo@basis.com.br>
2020-08-04 23:08:00 -0300

    BASIS-135172 - Alterar classes de teste.

--- src/test/java/br/com/basis/abaco/cucumber/CucumberIT.java
+++ src/test/java/br/com/basis/abaco/cucumber/CucumberIT.java
@@ -8,6 +8,6 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = "pretty", features = "src/test/features")
-public class CucumberTest  {
+public class CucumberIT {
 
 }
