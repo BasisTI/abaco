e0cda75a2a7a9b2a5f0700dd1054a73bd0e41bba
edivaldo.melo <edivaldo.melo@basis.com.br>
2020-09-01 18:28:27 -0300

    BASIS-146601 - Incluir controle de status nas analises

--- src/app/status/status.module.ts
+++ src/app/status/status.module.ts
@@ -1,4 +1,3 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
@@ -15,20 +14,26 @@ import { DatatableModule } from '@nuvem/primeng-components';
 import { AbacoButtonsModule } from '../components/abaco-buttons/abaco-buttons.module';
 import { SharedModule } from '../shared/shared.module';
 import { AdminGuard } from '../util/admin.guard';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/compiler';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CheckboxModule } from 'primeng';
 
 
 @NgModule({
   imports: [
-    CommonModule,
-    HttpClientModule,
-    FormsModule,
-    RouterModule.forRoot(statusRoute, { useHash: true }),
-    DatatableModule,
-    AbacoButtonsModule,
-    SharedModule,
-    CheckboxModule,
+      CommonModule,
+      FormsModule,
+      RouterModule.forRoot(statusRoute, {useHash: true}),
+      DatatableModule,
+      AbacoButtonsModule,
+      SharedModule,
+      CommonModule,
+      HttpClientModule,
+      FormsModule,
+      RouterModule.forRoot(statusRoute, { useHash: true }),
+      DatatableModule,
+      AbacoButtonsModule,
+      SharedModule,
+      CheckboxModule,
   ],
   declarations: [
     StatusListComponent,
@@ -37,8 +42,9 @@ import { CheckboxModule } from 'primeng';
   ],
   providers: [
     StatusService,
-    AdminGuard
+    AdminGuard,
   ],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class StatusModule { }
+export class StatusModule {
+}
