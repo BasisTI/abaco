6d91d5af80475a62f3ba78e7f4c7d2e01f160bb1
edivaldo.melo <edivaldo.melo@basis.com.br>
2020-11-05 09:49:07 -0300

    BASIS-157029 - Sonar Quality

--- src/main/java/br/com/basis/abaco/service/dto/FuncaoDadosEditSaveDTO.java
+++ src/main/java/br/com/basis/abaco/service/dto/FuncaoDadosEditSaveDTO.java
@@ -1,32 +1,23 @@
 package br.com.basis.abaco.service.dto;
 
 import br.com.basis.abaco.domain.Alr;
-import br.com.basis.abaco.domain.Der;
 import br.com.basis.abaco.domain.FatorAjuste;
 import br.com.basis.abaco.domain.Funcionalidade;
-import br.com.basis.abaco.domain.Rlr;
 import br.com.basis.abaco.domain.UploadedFile;
 import br.com.basis.abaco.domain.enumeration.Complexidade;
 import br.com.basis.abaco.domain.enumeration.StatusFuncao;
 import br.com.basis.abaco.domain.enumeration.TipoFuncaoDados;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
-
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class FuncaoDadosEditDTO extends FuncaoDadosEditSaveDTO{
-
+public class FuncaoDadosEditSaveDTO {
     private Long id;
     private Complexidade complexidade;
     private BigDecimal pf;
@@ -44,9 +35,4 @@ public class FuncaoDadosEditDTO extends FuncaoDadosEditSaveDTO{
     private Alr alr;
     private List<UploadedFile> files = new ArrayList<>();
     private Set<String> rlrValues = new HashSet<>();
-    private Set<Rlr> rlrs = new HashSet<>();
-    private Set<Der> ders = new LinkedHashSet<>();
-
-
-
 }
