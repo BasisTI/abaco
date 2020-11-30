a59a625c198d6a40524ee4dbde92fc85d39decbf
edivaldo.melo <edivaldo.melo@basis.com.br>
2020-11-29 21:10:27 -0300

    BASIS-159301 - Erro ao atualizar a baseline- update Liquibase - Sonar quality

--- src/main/java/br/com/basis/abaco/service/BaselineSinteticoService.java
+++ src/main/java/br/com/basis/abaco/service/BaselineSinteticoService.java
@@ -21,6 +21,8 @@ public class BaselineSinteticoService {
     private static final String SELECT_FUNCTION_FD = "SELECT f.row_number, f.classificacao, f.impacto , f.analise_id, f.data_homologacao_software, f.id_sistema, f.nome, f.sigla, f.name , f.pf, f.id_funcao_dados, f.complexidade, f.nome_equipe, f.equipe_responsavel_id, f.nome_funcionalidade, f.nome_modulo, f.der, f.rlr_alr, f.row_number, f.classificacao, f.impacto , f.analise_id, f.data_homologacao_software, f.id_sistema, f.nome, f.sigla, f.name, f.pf, f.id_funcao_dados, f.complexidade, f.nome_equipe, f.equipe_responsavel_id, f.nome_funcionalidade, f.nome_modulo, f.der, f.rlr_alr from fc_funcao_dados_vw( :id , :idEquipe ) f;";
     private static final String SELECT_FUNCTION_FT = "SELECT f.row_number, f.classificacao, f.impacto , f.analise_id, f.data_homologacao_software, f.id_sistema, f.nome, f.sigla, f.name , f.pf, f.id_funcao_dados, f.complexidade, f.nome_equipe, f.equipe_responsavel_id, f.nome_funcionalidade, f.nome_modulo, f.der, f.rlr_alr, f.row_number, f.classificacao, f.impacto , f.analise_id, f.data_homologacao_software, f.id_sistema, f.nome, f.sigla, f.name, f.pf, f.id_funcao_dados, f.complexidade, f.nome_equipe, f.equipe_responsavel_id, f.nome_funcionalidade, f.nome_modulo, f.der, f.rlr_alr from fc_funcao_transacao_vw( :id , :idEquipe ) f; ";
     private static final String SELECT_SINTETICO = "SELECT f.row_number, f.id_sistema, f.sigla, f.nome, f.numero_ocorrencia, f.sum, f.equipe_responsavel_id, f.nome_equipe from fc_baseline_sintetico_vw( :id , :idEquipe ) f;";
+    private static final String ID_EQUIPE = "idEquipe";
+    private static final String ID = "id";
     private final BaseLineSinteticoSearchRepository baseLineSinteticoSearchRepository;
     private final BaseLineAnaliticoFDSearchRepository baseLineAnaliticoFDSearchRepository;
     private final BaseLineAnaliticoFTSearchRepository baseLineAnaliticoFTSearchRepository;
@@ -41,7 +43,7 @@ public class BaselineSinteticoService {
 
 
     public BaseLineSintetico getBaseLineSintetico(Long id, Long idEquipe) {
-        Query query = entityManager.createNativeQuery(SELECT_SINTETICO, BaseLineSintetico.class).setParameter("id", id).setParameter("idEquipe", idEquipe);
+        Query query = entityManager.createNativeQuery(SELECT_SINTETICO, BaseLineSintetico.class).setParameter(ID, id).setParameter(ID_EQUIPE, idEquipe);
         BaseLineSintetico baseLineSintetico = (BaseLineSintetico) query.getSingleResult();
         return baseLineSintetico;
     }
@@ -50,8 +52,8 @@ public class BaselineSinteticoService {
         BaseLineSintetico result = baseLineSinteticoSearchRepository.save(baseLineSintetico);
         baseLineAnaliticoFDSearchRepository.deleteAllByIdsistemaAndEquipeResponsavelId(id, idEquipe);
         baseLineAnaliticoFTSearchRepository.deleteAllByIdsistemaAndEquipeResponsavelId(id, idEquipe);
-        Query nativeQueryFD = entityManager.createNativeQuery(SELECT_FUNCTION_FD, BaseLineAnaliticoFD.class).setParameter("id", id).setParameter("idEquipe", idEquipe);
-        Query nativeQueryFT = entityManager.createNativeQuery(SELECT_FUNCTION_FT, BaseLineAnaliticoFT.class).setParameter("id", id).setParameter("idEquipe", idEquipe);
+        Query nativeQueryFD = entityManager.createNativeQuery(SELECT_FUNCTION_FD, BaseLineAnaliticoFD.class).setParameter(ID, id).setParameter(ID_EQUIPE, idEquipe);
+        Query nativeQueryFT = entityManager.createNativeQuery(SELECT_FUNCTION_FT, BaseLineAnaliticoFT.class).setParameter(ID, id).setParameter(ID_EQUIPE, idEquipe);
         List<BaseLineAnaliticoFT> lstAnaliticoFT = nativeQueryFT.getResultList();
         List<BaseLineAnaliticoFD> lstAnaliticoFD = nativeQueryFD.getResultList();
         lstAnaliticoFD.forEach(baseLineAnaliticoFD -> baseLineAnaliticoFDSearchRepository.save(baseLineAnaliticoFD));
