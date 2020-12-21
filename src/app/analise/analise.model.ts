17bbe92a8654ffabfc8f4c35085cfa64c547a123
edivaldo.melo <edivaldo.melo@basis.com.br>
2020-12-20 22:23:40 -0300

    BASIS-166670 - Não é possível acessar validação

--- src/app/analise/analise.model.ts
+++ src/app/analise/analise.model.ts
@@ -78,6 +78,7 @@ export class Analise implements BaseEntity {
         public manual?: Manual,
         public users?: User[],
         public status?: Status,
+        public analisesComparadas?: Analise[],
     ) {
         this.inicializaMappables(funcaoDados, funcaoTransacaos);
         this.inicializaResumos();
