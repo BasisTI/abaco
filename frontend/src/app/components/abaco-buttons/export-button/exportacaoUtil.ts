cd4a15d2ea4f6822ae881d21566fd77df1b1a27f
Claéber de Santana Felinto <claeber.felinto@basis.com.br>
2021-02-19 16:27:52 +0000

    Resolve BASIS-175020

--- frontend/src/app/components/abaco-buttons/export-button/exportacaoUtil.ts
+++
@@ -1,9 +0,0 @@
-export class ExportacaoUtil {
-    static download(url, fileName: string) {
-        const anchor = document.createElement('a');
-        anchor.download = fileName;
-        anchor.href = url;
-        document.body.appendChild(anchor);
-        anchor.click();
-    }
-}
