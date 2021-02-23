3e8c3e34060e429736e09d9517c47d5fb16641f8
CSF <claeber.felinto@basis.com.br>
2021-02-23 18:01:29 -0300

    Ajuste componente exportar BASIS-179395

--- backend/src/main/java/br/com/basis/abaco/service/relatorio/RelatorioAnaliseColunas.java
+++ backend/src/main/java/br/com/basis/abaco/service/relatorio/RelatorioAnaliseColunas.java
@@ -17,35 +17,17 @@ public class RelatorioAnaliseColunas extends PropriedadesRelatorio {
     private static final String EQUIPE_RESPONSAVEL_NOME = "equipeResponsavel.nome";
     private static final String IDENTIFICADOR_ANALISE = "identificadorAnalise";
     private static final String ORGANIZACAO_NOME = "organizacao.nome";
+    
+    private static final String[][] colunas = {{ORGANIZACAO_NOME, "Organização"},{IDENTIFICADOR_ANALISE,"Identificador"}, {EQUIPE_RESPONSAVEL_NOME,"Equipe"}, {SISTEMA_NOME,"Sistema"}
+    , {METODO_CONTAGEM_STRING,"Método de contagem"}, {PF_TOTAL,"PF Total"},{ ADJUST_PF_TOTAL,"PF Ajustado"},{ CREATED_ON,"Data Criação"},{ BLOQUEIA_STRING,"Bloqueada?"}};
+
 
     public RelatorioAnaliseColunas(List<String> colunasVisiveis) {
         super("Listagem das Analises", "Total de Analises");
-        if (colunasVisiveis == null || colunasVisiveis.contains(ORGANIZACAO_NOME)) {
-            super.getColunas().add(new ColunasPropriedadeRelatorio(ORGANIZACAO_NOME, "Organização", String.class, 10,DynamicExportsConstants.MASCARA_NULL, DynamicExportsConstants.ALINHAR_ESQUERDA));
-        }
-        if (colunasVisiveis == null || colunasVisiveis.contains(IDENTIFICADOR_ANALISE)) {
-            super.getColunas().add(new ColunasPropriedadeRelatorio(IDENTIFICADOR_ANALISE, "Identificador", String.class,10, DynamicExportsConstants.MASCARA_NULL, DynamicExportsConstants.ALINHAR_ESQUERDA));
-        }
-        if (colunasVisiveis == null || colunasVisiveis.contains(EQUIPE_RESPONSAVEL_NOME)) {
-            super.getColunas().add(new ColunasPropriedadeRelatorio(EQUIPE_RESPONSAVEL_NOME, "Equipe", String.class, 10,DynamicExportsConstants.MASCARA_NULL, DynamicExportsConstants.ALINHAR_ESQUERDA));
-        }
-        if (colunasVisiveis == null || colunasVisiveis.contains(SISTEMA_NOME)) {
-            super.getColunas().add(new ColunasPropriedadeRelatorio(SISTEMA_NOME, "Sistema", String.class, 10,DynamicExportsConstants.MASCARA_NULL, DynamicExportsConstants.ALINHAR_ESQUERDA));
-        }
-        if (colunasVisiveis == null || colunasVisiveis.contains(METODO_CONTAGEM_STRING)) {
-            super.getColunas().add(new ColunasPropriedadeRelatorio(METODO_CONTAGEM_STRING, "Método de contagem",String.class, 10, DynamicExportsConstants.MASCARA_NULL, DynamicExportsConstants.ALINHAR_ESQUERDA));
-        }
-        if (colunasVisiveis == null || colunasVisiveis.contains(PF_TOTAL)) {
-            super.getColunas().add(new ColunasPropriedadeRelatorio(PF_TOTAL, "PF Total", String.class, 10,DynamicExportsConstants.MASCARA_NULL, DynamicExportsConstants.ALINHAR_DIREITA));
-        }
-        if (colunasVisiveis == null || colunasVisiveis.contains(ADJUST_PF_TOTAL)) {
-            super.getColunas().add(new ColunasPropriedadeRelatorio(ADJUST_PF_TOTAL, "PF Ajustado", String.class, 10,DynamicExportsConstants.MASCARA_NULL, DynamicExportsConstants.ALINHAR_DIREITA));
-        }
-        if (colunasVisiveis == null || colunasVisiveis.contains(CREATED_ON)) {
-            super.getColunas().add(new ColunasPropriedadeRelatorio(CREATED_ON, "Data Criação", String.class, 10,DynamicExportsConstants.MASCARA_NULL, DynamicExportsConstants.ALINHAR_DIREITA));
-        }
-        if (colunasVisiveis == null || colunasVisiveis.contains(BLOQUEIA_STRING)) {
-            super.getColunas().add(new ColunasPropriedadeRelatorio(BLOQUEIA_STRING, "Bloqueada?", String.class, 10,DynamicExportsConstants.MASCARA_NULL, DynamicExportsConstants.ALINHAR_ESQUERDA));
+        for (String[] string : colunas) {
+            if(colunasVisiveis.contains(string[0])) {
+                super.getColunas().add(new ColunasPropriedadeRelatorio(string[0], string[1], String.class, 10, DynamicExportsConstants.MASCARA_NULL, DynamicExportsConstants.ALINHAR_ESQUERDA));
+            }
         }
     }
 }
