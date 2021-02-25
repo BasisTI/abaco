cf13492aeb311df3612e352ec60eb7db86355bab
Claéber de Santana Felinto <claeber.felinto@basis.com.br>
2021-02-25 16:42:42 +0000

    Resolve BASIS-170690

--- frontend/src/app/components/topbar/app.topbar.component.ts
+++ frontend/src/app/components/topbar/app.topbar.component.ts
@@ -1,23 +1,43 @@
 import { Component } from '@angular/core';
 import { AppComponent } from '../../app.component';
-import { Authentication, User} from '@nuvem/angular-base';
+import { AuthenticationService } from '@nuvem/angular-base';
+import { User } from '../../user/user.model';
+
 
 @Component({
     selector: 'app-topbar',
     templateUrl: './app.topbar.component.html',
     styleUrls: ['./app.topbar.component.css']
 })
+
 export class AppTopbarComponent {
 
-    constructor(public app: AppComponent, private readonly _authentication: Authentication<User>
-      ) {
-    }
+
+    constructor(public app: AppComponent,
+
+        private readonly _authentication:
+
+
+        AuthenticationService<User>)    {
+        }
 
     get usuario() {
-        return this._authentication.getUser().name;
+        return this._authentication.getUser();
     }
 
     isAuthenticated() {
         return this._authentication.isAuthenticated();
+      }
+    AuthenticationService() {
+        return this._authentication.isAuthenticated();
+    }
+    authenticatedUserFirstName(): string {
+        const storageUser  = this._authentication.getUser();
+        if (!storageUser) {
+            return null;
+        }
+
+        return storageUser.firstName;
     }
 }
+
