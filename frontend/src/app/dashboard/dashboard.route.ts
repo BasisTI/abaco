77c54d2297e44afad354948be5dbca9f357f9a34
pedro.h.santos <pedro.h.santos@basis.com.br>
2021-03-24 10:41:23 -0300

    Ajustar rotas para breadcrumb funcionar - BASIS-185388

--- frontend/src/app/dashboard/dashboard.route.ts
+++ frontend/src/app/dashboard/dashboard.route.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { DashboardModule } from './dashboard.module';
 import { DashboardComponent } from './dashboard.component';
+import { AuthGuardService } from '../util/auth.guard.service';
 
 
 export const dashboardRoute: Routes = [
