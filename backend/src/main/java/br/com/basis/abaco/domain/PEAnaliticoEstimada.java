2125646d3caf65165921850a3c647424507be3fc
Pedro Henrique Fernandes Santos <pedro.h.santos@basis.com.br>
2021-06-11 12:57:06 +0000

    Adicionar contagens estimadas para importação de fd/ft - BASIS-203314

--- backend/src/main/java/br/com/basis/abaco/domain/PEAnaliticoEstimada.java
+++ backend/src/main/java/br/com/basis/abaco/domain/PEAnaliticoEstimada.java
@@ -24,26 +24,17 @@ public class PEAnaliticoEstimada implements Serializable {
     @Column(name = "row_number")
     private Long id;
 
-    @Column(name = "id_funcao_dados")
-    private Long idfuncaodados;
-
     @Column(name = "id_sistema")
     private Long idsistema;
 
     @Column(name = "equipe_responsavel_id")
     private Long equipeResponsavelId;
 
+    @Column(name = "id_funcao_dados")
+    private Long idfuncaodados;
+
     @Column(name = "tipo")
     private String tipo;
-
-    @Column(name = "classificacao")
-    private String classificacao;
-
-    @Column(name = "analise_id")
-    private Long analiseid;
-
-    @Column(name = "data_homologacao_software")
-    private String dataHomologacao;
 
     @Column(name = "nome")
     private String nome;
@@ -51,17 +42,17 @@ public class PEAnaliticoEstimada implements Serializable {
     @Column(name = "nome_equipe")
     private String nomeEquipe;
 
+    @Column(name = "classificacao")
+    private String classificacao;
+
+    @Column(name = "data_homologacao_software")
+    private String dataHomologacao;
+
+    @Column(name = "analise_id")
+    private Long analiseid;
+
     @Column(name = "sigla")
     private String sigla;
-
-    @Column(name = "name")
-    private String name;
-
-    @Column(name = "pf")
-    private BigDecimal pf;
-
-    @Column(name = "complexidade")
-    private String complexidade;
 
     @Column(name = "id_funcionalidade")
     private Long idFuncionalidade;
@@ -69,10 +60,19 @@ public class PEAnaliticoEstimada implements Serializable {
     @Column(name = "nome_funcionalidade")
     private String nomeFuncionalidade;
 
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "pf")
+    private BigDecimal pf;
+
     @Column(name = "id_modulo")
     private Long idModulo;
 
     @Column(name = "nome_modulo")
     private String nomeModulo;
+
+    @Column(name = "complexidade")
+    private String complexidade;
 
 }
