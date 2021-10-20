50661ed20e7a2878c631f752196d9ffabbc45093
fabrica.s2 <fabrica.s2@basis.com.br>
2021-10-20 14:19:36 -0300

    Não permitir que módulos sejam duplicados - BASIS-231615

--- backend/src/main/java/br/com/basis/abaco/repository/ModuloRepository.java
+++ backend/src/main/java/br/com/basis/abaco/repository/ModuloRepository.java
@@ -4,6 +4,8 @@ import br.com.basis.abaco.domain.Modulo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
+
 /**
  * Spring Data JPA repository for the Modulo entity.
  */
@@ -13,4 +15,5 @@ public interface ModuloRepository extends JpaRepository<Modulo, Long> {
     @Query(value = "SELECT m FROM Modulo m WHERE m.id = ( SELECT f.modulo.id FROM Funcionalidade f WHERE f.id = ?1 )")
     public Modulo findByFuncionalidade(Long id);
 
+    Optional<Modulo> findAllByNomeAndSistemaId(String nome, Long sistemaId);
 }
